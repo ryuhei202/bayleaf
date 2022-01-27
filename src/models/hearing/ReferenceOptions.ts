53cdf3fd0050ed991d8e0a427afcfb46044ee540
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-01-27 09:22:48 +0900

    ReferenceOptionsのキーの型を定義

--- src/models/hearing/ReferenceOptions.ts
+++ src/models/hearing/ReferenceOptions.ts
@@ -1,7 +1,11 @@
 export namespace ReferenceOptions {
-  type TOption = { [key: string]: { id: number; name: string } };
+  type TOption<K extends string> = {
+    [key in K]: { id: number; name: string };
+  };
 
-  export const TARGET: TOption = {
+  export const TARGET: TOption<
+    "COWORKER" | "FAMILY" | "PARTNER" | "FRIEND" | "NONE"
+  > = {
     COWORKER: { id: 1, name: "職場" },
     FAMILY: { id: 2, name: "家族" },
     PARTNER: { id: 3, name: "異性(恋人)" },
@@ -9,7 +13,9 @@ export namespace ReferenceOptions {
     NONE: { id: 5, name: "特になし" },
   };
 
-  export const MULTIPLE_IMPRESSIONS: TOption = {
+  export const MULTIPLE_IMPRESSIONS: TOption<
+    "KIND" | "YOUNG" | "AGE_FIT" | "CLEAN" | "CALM" | "ACTIVE"
+  > = {
     KIND: { id: 6, name: "優しい" },
     YOUNG: { id: 7, name: "若々しい" },
     AGE_FIT: { id: 8, name: "年齢に合った" },
@@ -18,7 +24,9 @@ export namespace ReferenceOptions {
     ACTIVE: { id: 11, name: "活発な(アクティブな)" },
   };
 
-  export const PRIMARY_IMPRESSIONS: TOption = {
+  export const PRIMARY_IMPRESSIONS: TOption<
+    "KIND" | "YOUNG" | "AGE_FIT" | "CLEAN" | "CALM" | "ACTIVE"
+  > = {
     KIND: { id: 12, name: "優しい" },
     YOUNG: { id: 13, name: "若々しい" },
     AGE_FIT: { id: 14, name: "年齢に合った" },
@@ -27,14 +35,18 @@ export namespace ReferenceOptions {
     ACTIVE: { id: 17, name: "活発な(アクティブな)" },
   };
 
-  export const CASUAL_SLEEVE: TOption = {
+  export const CASUAL_SLEEVE: TOption<
+    "LONG_3_SHORT_0" | "LONG_2_SHORT_1" | "LONG_1_SHORT_2" | "LONG_0_SHORT_3"
+  > = {
     LONG_3_SHORT_0: { id: 23, name: "長袖3 / 半袖0" },
     LONG_2_SHORT_1: { id: 24, name: "長袖2 / 半袖1" },
     LONG_1_SHORT_2: { id: 25, name: "長袖1 / 半袖2" },
     LONG_0_SHORT_3: { id: 26, name: "長袖0 / 半袖3" },
   };
 
-  export const BUSINESS_SLEEVE: TOption = {
+  export const BUSINESS_SLEEVE: TOption<
+    "LONG_2_SHORT_0" | "LONG_1_SHORT_1" | "LONG_0_SHORT_2"
+  > = {
     LONG_2_SHORT_0: { id: 27, name: "長袖2 / 半袖0" },
     LONG_1_SHORT_1: { id: 28, name: "長袖1 / 半袖1" },
     LONG_0_SHORT_2: { id: 29, name: "長袖0 / 半袖2" },
