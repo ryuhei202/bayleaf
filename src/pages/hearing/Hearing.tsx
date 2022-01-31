67891d262cab35e1ad7f858d07bfbfd724e1ee1f
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-01-31 13:54:59 +0900

    試験用の通知を実装

--- src/pages/hearing/Hearing.tsx
+++ src/pages/hearing/Hearing.tsx
@@ -1,3 +1,4 @@
+import Bugsnag from "@bugsnag/js";
 import { useEffect, useState } from "react";
 import { TMembersIndexResponse } from "../../api/members/TMembersIndexResponse";
 import { MemberListContainer } from "./MemberListContainer";
@@ -6,6 +7,7 @@ import { ReferenceFetcher } from "./ReferenceFetcher";
 export const Hearing = () => {
   const [member, setMember] =
     useState<TMembersIndexResponse | undefined>(undefined);
+  Bugsnag.notify("エラー通知テスト");
 
   useEffect(() => {
     document.title = "ヒアリング | leeap";
