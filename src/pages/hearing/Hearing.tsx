5ca9389071b4af65a578e8e2ddf301678eab6b84
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-01-31 14:01:52 +0900

    Revert "試験用の通知を実装"
    This reverts commit 67891d262cab35e1ad7f858d07bfbfd724e1ee1f.

--- src/pages/hearing/Hearing.tsx
+++ src/pages/hearing/Hearing.tsx
@@ -1,4 +1,3 @@
-import Bugsnag from "@bugsnag/js";
 import { useEffect, useState } from "react";
 import { TMembersIndexResponse } from "../../api/members/TMembersIndexResponse";
 import { MemberListContainer } from "./MemberListContainer";
@@ -7,7 +6,6 @@ import { ReferenceFetcher } from "./ReferenceFetcher";
 export const Hearing = () => {
   const [member, setMember] =
     useState<TMembersIndexResponse | undefined>(undefined);
-  Bugsnag.notify("エラー通知テスト");
 
   useEffect(() => {
     document.title = "ヒアリング | leeap";
