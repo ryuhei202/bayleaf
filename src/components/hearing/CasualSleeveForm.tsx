29c9cc398b6414d06dc071ead2a9e0e090632419
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-02-01 14:11:31 +0900

    iphoneの表示崩れを修正

--- src/components/hearing/CasualSleeveForm.tsx
+++ src/components/hearing/CasualSleeveForm.tsx
@@ -84,7 +84,7 @@ export const CasualSleeveForm = ({
             </SelectButton>
           </div>
         </div>
-        <div className="">
+        {/* <div className="mb-10 flex flex-row space-x-3">
           {onCancel ? (
             <IconButton className="flex-none" onClick={onCancel}>
               <ArrowIcon className="h-10 w-fit my-auto" />
@@ -104,7 +104,7 @@ export const CasualSleeveForm = ({
           ) : (
             <></>
           )}
-        </div>
+        </div> */}
       </div>
     </Page>
   );
