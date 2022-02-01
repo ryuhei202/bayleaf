7b12d460c2c703c8ad090b28f66b3bdd92d61985
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-02-01 14:27:19 +0900

    iphoneの表示崩れを修正

--- src/components/hearing/CasualSleeveForm.tsx
+++ src/components/hearing/CasualSleeveForm.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { ReferenceOptions } from "../../models/hearing/ReferenceOptions";
+import { Button } from "../baseParts/Button";
+import { IconButton } from "../baseParts/IconButton";
+import { ArrowIcon } from "../baseParts/icons/ArrowIcon";
 import { LongSleeveIcon } from "../baseParts/icons/LongSleeveIcon";
 import { ShortSleeveIcon } from "../baseParts/icons/ShortSleeveIcon";
 import { Page } from "../baseParts/Page";
@@ -81,7 +84,7 @@ export const CasualSleeveForm = ({
             </SelectButton>
           </div>
         </div>
-        {/* <div className="mb-10 flex flex-row space-x-3">
+        <div className="w-full flex flex-row mb-10">
           {onCancel ? (
             <IconButton className="flex-none" onClick={onCancel}>
               <ArrowIcon className="h-10 w-fit my-auto" />
@@ -93,7 +96,7 @@ export const CasualSleeveForm = ({
             <Button
               disabled={defaultValue === value}
               size="none"
-              className="grow"
+              className="grow ml-3"
               onClick={handleSubmit}
             >
               <Typography className="my-auto">変更を適用する</Typography>
@@ -101,7 +104,7 @@ export const CasualSleeveForm = ({
           ) : (
             <></>
           )}
-        </div> */}
+        </div>
       </div>
     </Page>
   );
