5813f442e1cf9e0171beed5367d61ca3cd27a1fe
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-04-26 18:19:14 +0900

    SelectButtonImageを実装

---
+++ src/components/baseParts/images/SelectButtonImage.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { SelectButtonImage } from "./SelectButtonImage";
+
+export default {
+  title: "BaseParts/Images/SelectButtonImage",
+  component: SelectButtonImage,
+} as ComponentMeta<typeof SelectButtonImage>;
+
+const Template: ComponentStory<typeof SelectButtonImage> = (args) => (
+  <SelectButtonImage {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  imageSrc: "https://placehold.jp/ca161c/ffffff/198x300.png",
+};
+Default.decorators = [
+  (Story) => (
+    <div className="bg-slate-200 w-full p-3">
+      <div className="w-20">
+        <Story />
+      </div>
+    </div>
+  ),
+];
---
+++ src/components/baseParts/images/SelectButtonImage.tsx
@@ -0,0 +1,26 @@
+import { ImageAlt } from "./ImageAlt";
+
+type TProps = {
+  imageSrc: string;
+  value: string;
+  onChange: () => void;
+  className?: string;
+};
+export const SelectButtonImage = ({
+  imageSrc,
+  value,
+  onChange,
+  className,
+}: TProps) => {
+  return (
+    <div className={`${className ?? ""} w-full relative`}>
+      <input
+        type="checkbox"
+        className="form-checkbox absolute right-1.5 bottom-1.5 rounded-full text-gray-600 focus:outline-none h-5 w-5"
+        value={value}
+        onChange={onChange}
+      />
+      <ImageAlt imageSrc={imageSrc} />
+    </div>
+  );
+};
