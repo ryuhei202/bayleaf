6f957ab142f042c5da09499ceca98b8476d35b5b
SoTakano <s-takano@kiizan-kiizan.co.jp>
2022-04-26 12:41:21 +0900

    TKarteIndexResponse削除

--- src/api/kartes/TKarteIndexResponse.ts
+++
@@ -1,8 +0,0 @@
-export type TKarteIndexResponse = {
-  readonly kartes: {
-    readonly id: number;
-    readonly rentalStartedAt: string;
-    readonly itemImagePaths: string[];
-    readonly planName: string;
-  }[];
-};
