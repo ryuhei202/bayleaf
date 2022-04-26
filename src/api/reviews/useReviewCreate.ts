095c4e5681f72afc38475bd89e83e45e080d90f2
SoTakano <s-takano@kiizan-kiizan.co.jp>
2022-04-26 12:40:11 +0900

    useReviewCreate実装

---
+++ src/api/reviews/useReviewCreate.ts
@@ -0,0 +1,28 @@
+import { AxiosResponse } from "axios";
+import { UseMutateFunction } from "react-query";
+import { usePostRequest } from "../usePostRequest";
+
+type TReviewCreate = {
+  readonly mutate: UseMutateFunction<
+    AxiosResponse,
+    unknown,
+    TReviewCreateParams,
+    unknown
+  >;
+  readonly isLoading: boolean;
+};
+
+export type TReviewCreateParams = {
+  readonly reviews: {
+    readonly coordinateId: number;
+    readonly choiceId: number;
+    readonly reasonChoiceIds: number[];
+    readonly text: string;
+  }[];
+};
+
+export const useReviewCreate = (): TReviewCreate => {
+  const { mutate, isLoading } = usePostRequest<TReviewCreateParams>(`reviews`);
+
+  return { mutate, isLoading };
+};
