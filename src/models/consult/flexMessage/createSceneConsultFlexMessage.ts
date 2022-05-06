dfe3181f4666119c8f0ecf7d266e649209c9faac
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-05-06 16:11:04 +0900

    シーンのFlexメッセージをカルーセルからバブルに修正

--- src/models/consult/flexMessage/createSceneConsultFlexMessage.ts
+++ src/models/consult/flexMessage/createSceneConsultFlexMessage.ts
@@ -7,70 +7,65 @@ type TSceneAnswer = {
  * 着こなし相談のシーンFlexメッセージをJSON文字列で返却
  */
 export const createSceneConsultFlexMessage = (
-  formAnswers: TSceneAnswer[]
+  formAnswer: TSceneAnswer
 ): string => {
   let flexMessage = {
-    type: "carousel",
-    contents: formAnswers.map((answer) => {
-      return {
-        type: "bubble",
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [
-            {
-              type: "text",
-              text: "■相談内容",
-              weight: "bold",
-              size: "sm",
-              wrap: true,
-            },
-            {
-              type: "text",
-              text: "使うシーンに合っているか気になる",
-              size: "xl",
-              margin: "md",
-              wrap: true,
-            },
-            {
-              type: "text",
-              text: "■相談コーデ",
-              weight: "bold",
-              size: "sm",
-              margin: "md",
-              wrap: true,
-            },
-            {
-              type: "box",
-              layout: "horizontal",
-              contents: answer.itemImageUrls.map((imageUrl) => {
-                return {
-                  type: "image",
-                  url: imageUrl,
-                };
-              }),
-              margin: "md",
-              alignItems: "center",
-            },
-            {
-              type: "text",
-              text: "■使いたいシーンについて",
-              margin: "lg",
-              weight: "bold",
-              size: "sm",
-              wrap: true,
-            },
-            {
-              type: "text",
-              text: answer.freetext,
-              margin: "md",
-              size: "xxs",
-              wrap: true,
-            },
-          ],
+    type: "bubble",
+    body: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "text",
+          text: "■相談内容",
+          weight: "bold",
+          size: "sm",
+          wrap: true,
         },
-      };
-    }),
+        {
+          type: "text",
+          text: "使うシーンに合っているか気になる",
+          size: "xl",
+          margin: "md",
+          wrap: true,
+        },
+        {
+          type: "text",
+          text: "■相談コーデ",
+          weight: "bold",
+          size: "sm",
+          margin: "md",
+          wrap: true,
+        },
+        {
+          type: "box",
+          layout: "horizontal",
+          contents: formAnswer.itemImageUrls.map((imageUrl) => {
+            return {
+              type: "image",
+              url: imageUrl,
+            };
+          }),
+          margin: "md",
+          alignItems: "center",
+        },
+        {
+          type: "text",
+          text: "■使いたいシーンについて",
+          margin: "lg",
+          weight: "bold",
+          size: "sm",
+          wrap: true,
+        },
+        {
+          type: "text",
+          text: formAnswer.freetext,
+          margin: "md",
+          size: "xxs",
+          wrap: true,
+        },
+      ],
+    },
   };
 
   return JSON.stringify(flexMessage);
