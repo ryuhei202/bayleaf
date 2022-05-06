a99a7ac11e7b50480adb218a41b8bf97b84cafb3
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-05-06 16:11:50 +0900

    私物相談flexメッセージ作成関数を実装

---
+++ src/models/consult/flexMessage/createCombinationConsultFlexMessage.ts
@@ -0,0 +1,99 @@
+type TCombinationAnswer = {
+  itemImageUrls: string[];
+  personalItem?: {
+    category: string;
+    color: string;
+    additionalText?: string;
+  };
+};
+
+/**
+ * 着こなし相談の私物FlexメッセージをJSON文字列で返却
+ */
+export const createCombinationConsultFlexMessage = (
+  formAnswer: TCombinationAnswer
+): string => {
+  let contents = [
+    {
+      type: "text",
+      text: "■相談内容",
+      weight: "bold",
+      size: "sm",
+      wrap: true,
+    },
+    {
+      type: "text",
+      text: "靴やアウターなど私物との組み合わせが気になる",
+      size: "xl",
+      margin: "md",
+      wrap: true,
+    },
+    {
+      type: "text",
+      text: "■相談コーデ",
+      weight: "bold",
+      size: "sm",
+      margin: "md",
+      wrap: true,
+    },
+    {
+      type: "box",
+      layout: "horizontal",
+      contents: formAnswer.itemImageUrls.map((imageUrl) => {
+        return {
+          type: "image",
+          url: imageUrl,
+        };
+      }),
+      margin: "md",
+      alignItems: "center",
+    },
+  ];
+
+  if (formAnswer.personalItem !== undefined) {
+    contents.push({
+      type: "text",
+      text: "■使いたいアイテム",
+      margin: "lg",
+      weight: "bold",
+      size: "sm",
+      wrap: true,
+    });
+    contents.push({
+      type: "text",
+      text: `${formAnswer.personalItem.category}／${formAnswer.personalItem.color}`,
+      margin: "md",
+      size: "xxs",
+      wrap: true,
+    });
+
+    if (formAnswer.personalItem.additionalText !== undefined) {
+      contents.push({
+        type: "text",
+        text: "■使いたいアイテムの詳細情報",
+        margin: "lg",
+        weight: "bold",
+        size: "sm",
+        wrap: true,
+      });
+      contents.push({
+        type: "text",
+        text: `${formAnswer.personalItem.additionalText}`,
+        margin: "md",
+        size: "xxs",
+        wrap: true,
+      });
+    }
+  }
+
+  let flexMessage = {
+    type: "bubble",
+    body: {
+      type: "box",
+      layout: "vertical",
+      contents,
+    },
+  };
+
+  return JSON.stringify(flexMessage);
+};
