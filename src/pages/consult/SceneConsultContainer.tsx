8a7a8b6b09b13af8f5e292b0ccd68abacb5217b4
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-05-09 16:15:12 +0900

    相談内容の選択ができるように実装

---
+++ src/pages/consult/SceneConsultContainer.tsx
@@ -0,0 +1,9 @@
+import { TCoordinateResponse } from "../../api/coordinates/TCoordinateResponse";
+
+type TProps = {
+  coordinate: TCoordinateResponse;
+};
+
+export const SceneConsultContainer = ({ coordinate }: TProps) => {
+  return <div>SceneConsultContainer</div>;
+};
---
+++ src/pages/consult/SizeConsultContainer.tsx
@@ -0,0 +1,9 @@
+import { TCoordinateResponse } from "../../api/coordinates/TCoordinateResponse";
+
+type TProps = {
+  coordinate: TCoordinateResponse;
+};
+
+export const SizeConsultContainer = ({ coordinate }: TProps) => {
+  return <div>SizeConsultContainer</div>;
+};
