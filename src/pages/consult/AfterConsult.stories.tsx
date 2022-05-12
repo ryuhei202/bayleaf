1b513f5f89eac7760248104b5be04615a87c32d4
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-05-12 14:38:19 +0900

    feat: 相談に答え終えた後のページを実装

---
+++ src/pages/consult/AfterConsult.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { AfterConsult } from "./AfterConsult";
+
+export default {
+  title: "Consult/AfterConsult",
+  component: AfterConsult,
+} as ComponentMeta<typeof AfterConsult>;
+
+const Template: ComponentStory<typeof AfterConsult> = (args) => (
+  <AfterConsult {...args} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  title: (
+    <>
+      スタイリストからLINEで
+      <br />
+      ご相談内容を詳しく伺います！
+    </>
+  ),
+  subTitle:
+    "コーデを自信を持って着ていただけるように、お悩み内容を確認しスタイリストからご連絡させていただきます。",
+  btnText: "完了",
+  onClick: () => {},
+};
+
+Default.decorators = [(Story) => <Story />];
