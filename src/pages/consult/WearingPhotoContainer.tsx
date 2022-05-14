1bd7b7f9f2af84c09435cbb27b474d8139d69800
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-05-14 12:56:47 +0900

    refactor: 受け取るpropsをプレゼンテーションに必要なものに修正

--- src/pages/consult/WearingPhotoContainer.tsx
+++ src/pages/consult/WearingPhotoContainer.tsx
@@ -1,29 +1,68 @@
+import { AxiosResponse } from "axios";
 import { useState } from "react";
+
+import {
+  TMemberPhotoCreateParams,
+  TMemberPhotoCreateResponse,
+  useMemberPhotoCreate,
+} from "../../api/consult/useMemberPhotoCreate";
 import { TImagePathsResponse } from "../../api/shared/TImagePathsResponse";
 import { Page } from "../../components/baseParts/Page";
+import { useImageUploadHandler } from "../../hooks/handler/image/useImageUploadHandler";
+import { MEMBER_PHOTO_CATEGORY_ID } from "../../models/consult/MemberPhotoCategoryId";
 import { TConsultingItem } from "../../models/consult/TConsultingItem";
+import { AfterConsultContainer } from "./AfterConsultContainer";
 import { WearingPhoto } from "./WearingPhoto";
 
 type TProps = {
   items: TConsultingItem[];
+  flexMessage: string;
 };
 
-export const WearingPhotoContainer = ({ items }: TProps) => {
+export const WearingPhotoContainer = ({ items, flexMessage }: TProps) => {
   const [uploadedImagePaths, setUploadedImagePaths] =
     useState<TImagePathsResponse | undefined>(undefined);
 
+  if (uploadedImagePaths)
+    return (
+      <Page>
+        <AfterConsultContainer flexMessage={flexMessage} />
+      </Page>
+    );
+
+  const { imageFileName, imageData, preUploadImage, onChangeFile } =
+    useImageUploadHandler();
+  const { mutate, isLoading } = useMemberPhotoCreate();
+  const onSubmit = () => {
+    if (imageData && imageFileName) {
+      const params: TMemberPhotoCreateParams = {
+        image: {
+          memberPhotoCategoryId: MEMBER_PHOTO_CATEGORY_ID.WEARING,
+          imageData: imageData,
+          imageFileName: imageFileName,
+        },
+      };
+      mutate(params, {
+        onSuccess: (data: AxiosResponse<TMemberPhotoCreateResponse>) => {
+          if (data && data.data) {
+            setUploadedImagePaths(data.data.imagePaths);
+          }
+        },
+      });
+    }
+  };
+
   return (
     <Page>
-      {uploadedImagePaths ? (
-        {
-          /** 別タスクで実装*/
-        }
-      ) : (
-        <WearingPhoto
-          items={items}
-          setUploadedImagePaths={setUploadedImagePaths}
-        />
-      )}
+      <WearingPhoto
+        items={items}
+        preUploadImage={preUploadImage ?? ""}
+        imageFileName={imageFileName ?? ""}
+        imageData={imageData ?? ""}
+        onChangeFile={onChangeFile}
+        onSubmit={onSubmit}
+        isLoading={isLoading}
+      />
     </Page>
   );
 };
