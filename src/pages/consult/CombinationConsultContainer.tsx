95b41bb484cfaec625526759087b71b1256474b8
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-05-17 17:24:19 +0900

    remove: 使用していないファイルを削除

--- src/pages/consult/CombinationConsultContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { TCoordinateResponse } from "../../api/coordinates/TCoordinateResponse";
-
-type TProps = {
-  coordinate: TCoordinateResponse;
-};
-
-export const CombinationConsultContainer = ({ coordinate }: TProps) => {
-  return <div>CombinationConsultContainer</div>;
-};
--- src/pages/consult/SceneConsultContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { TCoordinateResponse } from "../../api/coordinates/TCoordinateResponse";
-
-type TProps = {
-  coordinate: TCoordinateResponse;
-};
-
-export const SceneConsultContainer = ({ coordinate }: TProps) => {
-  return <div>SceneConsultContainer</div>;
-};
