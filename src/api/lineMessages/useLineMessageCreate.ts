ee1fc6df175794499995ebee9bf9ad81e80b1b73
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-05-18 17:14:57 +0900

    feat: useLineMessageCreate関数を実装

---
+++ src/api/lineMessages/useLineMessageCreate.ts
@@ -0,0 +1,24 @@
+import { AxiosResponse } from "axios";
+import { UseMutateFunction } from "react-query";
+import { usePostRequest } from "../usePostRequest";
+
+type TLineMessageCreate = {
+  readonly mutate: UseMutateFunction<
+    AxiosResponse,
+    any,
+    TLineMessageCreateParams,
+    unknown
+  >;
+  readonly isLoading: boolean;
+};
+
+export type TLineMessageCreateParams = {
+  readonly messages: any[];
+};
+
+export const useLineMessageCreate = (): TLineMessageCreate => {
+  const { mutate, isLoading } =
+    usePostRequest<TLineMessageCreateParams>("line_messages");
+
+  return { mutate, isLoading };
+};
