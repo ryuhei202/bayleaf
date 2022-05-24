a73bbdad7ceb09771ef3f6e188289b4fe53f6460
SoTakano <s-takano@kiizan-kiizan.co.jp>
2022-05-24 19:51:20 +0900

    feat: reviewSkipのApi対応

---
+++ src/api/reviews/useReviewSkip.ts
@@ -0,0 +1,24 @@
+import { AxiosResponse } from "axios";
+import { UseMutateFunction } from "react-query";
+import { usePostRequest } from "../usePostRequest";
+
+type TReviewSkip = {
+  readonly mutate: UseMutateFunction<
+    AxiosResponse,
+    unknown,
+    TReviewSkipParams,
+    unknown
+  >;
+  readonly isLoading: boolean;
+};
+
+export type TReviewSkipParams = {
+  readonly chartId: number;
+};
+
+export const useReviewSkip = (): TReviewSkip => {
+  const { mutate, isLoading } =
+    usePostRequest<TReviewSkipParams>("reviews/skip");
+
+  return { mutate, isLoading };
+};
