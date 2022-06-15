a795cf57fc8771c90dd4b7ce66a51144d7dad9e6
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-06-15 19:33:02 +0900

    fix: event_labelにmemberIdを渡すように修正 <feature/2596/LEEAP-2653-初回ヒアリングgaで取れるようにボタンにイベント仕込む>

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,19 +1,12 @@
 export type TGAEvent = {
   readonly action: string;
   readonly category: string;
-  readonly label?: string;
   readonly memberId?: number;
 };
 
-export const analyzeEvent = ({
-  action,
-  category,
-  label,
-  memberId,
-}: TGAEvent) => {
+export const analyzeEvent = ({ action, category, memberId }: TGAEvent) => {
   window.gtag("event", action, {
     event_category: category,
-    event_label: label,
-    value: memberId,
+    event_label: String(memberId),
   });
 };
