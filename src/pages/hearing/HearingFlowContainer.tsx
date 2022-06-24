497b2472f86cbfd76f77234640722fbad2bac43a
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-06-24 12:05:11 +0900

    feat: ヒアリングを出し分けるコンポーネントを実装 <feature/2597/LEEAP-2732-2回目ヒアリング-hearingflowコンポーネントの実>

---
+++ src/pages/hearing/HearingFlowContainer.tsx
@@ -0,0 +1,77 @@
+import { TMembersIndexResponse } from "../../api/members/TMembersIndexResponse";
+import { Button } from "../../components/baseParts/Button";
+import { HearingAnswerConfirm } from "../../components/hearing/HearingAnswerConfirm";
+import { THearingAnswer } from "../../models/hearing/THearingAnswer";
+import { AnsweredHearing } from "./HearingContainer";
+import { HearingFormFetcher } from "./HearingFormFetcher";
+
+type TProps = {
+  readonly handleSubmitForm: () => void;
+  readonly handleCancelForm: () => void;
+  readonly handleTransitionSleeveForm: () => void;
+  readonly handleClickStart: () => void;
+  readonly confirmAnswers: THearingAnswer[];
+  readonly nextFormId: number | null;
+  readonly currentAnswerNumber: 1 | 2;
+  readonly firstAnsweredHearings: AnsweredHearing[];
+  readonly secondAnsweredHearings: AnsweredHearing[];
+  readonly isBackTransition: boolean;
+  readonly member: TMembersIndexResponse;
+};
+
+export const HearingFlowContainer = ({
+  handleSubmitForm,
+  handleCancelForm,
+  handleTransitionSleeveForm,
+  handleClickStart,
+  confirmAnswers,
+  nextFormId,
+  currentAnswerNumber,
+  firstAnsweredHearings,
+  secondAnsweredHearings,
+  isBackTransition,
+  member,
+}: TProps) => {
+  if (nextFormId === null || firstAnsweredHearings.length <= 0) {
+    return (
+      <HearingAnswerConfirm
+        title="前回のコーデ"
+        confirmAnswers={confirmAnswers}
+        footer={
+          <>
+            <Button
+              variant="primary"
+              onClick={handleTransitionSleeveForm}
+              border={true}
+            >
+              前回と同じ内容でコーデを作る
+            </Button>
+            <Button
+              onClick={handleClickStart}
+              variant="default"
+              disableElevation
+              border
+            >
+              ヒアリングを開始する
+            </Button>
+          </>
+        }
+      />
+    );
+  }
+
+  return (
+    <HearingFormFetcher
+      onSubmitForm={handleSubmitForm}
+      onCancelForm={handleCancelForm}
+      nextFormId={nextFormId}
+      previousAnsweredHearing={
+        currentAnswerNumber === 1
+          ? firstAnsweredHearings.slice(-1)[0]
+          : secondAnsweredHearings.slice(-1)[0]
+      }
+      isBackTransition={isBackTransition}
+      member={member}
+    />
+  );
+};
