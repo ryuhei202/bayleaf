a55d78144afe7c51655d256baf110beb9a5c3b8b
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-06-29 15:21:25 +0900

    fix: レンタル開始日をnull許容させる <feature/2597/LEEAP-2762-bayleaf-結合テスト>

--- src/api/charts/TChartResponse.ts
+++ src/api/charts/TChartResponse.ts
@@ -1,6 +1,6 @@
 export type TChartResponse = {
   readonly id: number;
-  readonly rentalStartedAt: string;
+  readonly rentalStartedAt: string | null;
   readonly itemImagePaths: string[];
   readonly planName: string;
   readonly planId: number;
