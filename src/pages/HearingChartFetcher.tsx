55db484a0309197248feb229e760ea7f430ec70c
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-07-01 17:36:34 +0900

    feat: 回答済みの場合はヒアリング内容を表示する <feature/2778/LEEAP-2778-bayleaf回答したヒアリングの内容を表示する>

--- src/pages/HearingChartFetcher.tsx
+++ src/pages/HearingChartFetcher.tsx
@@ -1,8 +1,6 @@
 import { Loader } from "semantic-ui-react";
 import { useChartIndex } from "../api/charts/useChartIndex";
 import { TMembersIndexResponse } from "../api/members/TMembersIndexResponse";
-import { Page } from "../components/baseParts/Page";
-import { PageHeader } from "../components/baseParts/PageHeader";
 import { ErrorMessage } from "../components/shared/ErrorMessage";
 import { NewHearingContainer } from "./hearing/NewHearingContainer";
 import { HearingFetcher } from "./hearing/HearingFetcher";
@@ -18,17 +16,6 @@ export const HearingChartFetcher = ({ member }: TProps) => {
     },
   });
 
-  if (!member.isLatestChartDelivered) {
-    return (
-      <Page>
-        <PageHeader
-          title="既に回答済みです。"
-          className="m-4"
-          subtitle="スタイリストが対応しますので、少々お待ちください。"
-        />
-      </Page>
-    );
-  }
   if (chartIndexError)
     return <ErrorMessage message={chartIndexError.message} />;
 
