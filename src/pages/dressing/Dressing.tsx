72deaf200ab1c2f129b2b8c6794538fba4e7a233
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-07-14 15:37:22 +0900

    feat: レンタル中のカルテが複数ある場合にカルテを選択するページを表示する <feature/2743/LEEAP-2842-着こなしページfront-dressing.tsxの実装>

--- src/pages/dressing/Dressing.tsx
+++ src/pages/dressing/Dressing.tsx
@@ -1,3 +1,59 @@
+import { useEffect, useState } from "react";
+import { Loader } from "semantic-ui-react";
+import { TChartResponse } from "../../api/charts/TChartResponse";
+import { useChartIndex } from "../../api/charts/useChartIndex";
+import { Page } from "../../components/baseParts/Page";
+import { Typography } from "../../components/baseParts/Typography";
+import { ChartList } from "../../components/chart/ChartList";
+import { ErrorMessage } from "../../components/shared/ErrorMessage";
+import { CHART_RENTAL_STATUS } from "../../models/chart/ChartRentalStatus";
+
 export const Dressing = () => {
-  return <></>;
+  const [selectedChart, setSelectedChart] =
+    useState<TChartResponse | undefined>(undefined);
+  const { data: chartIndexData, error: chartIndexError } = useChartIndex({
+    params: {
+      rentalStatus: [CHART_RENTAL_STATUS.WAIT_RENTAL_RETURN],
+    },
+  });
+
+  useEffect(() => {
+    document.title = `着こなしページ | leeap`;
+  }, []);
+
+  useEffect(() => {
+    if (chartIndexData === undefined) return;
+
+    if (chartIndexData.charts.length === 1)
+      setSelectedChart(chartIndexData.charts[0]);
+  }, [chartIndexData]);
+
+  if (chartIndexError)
+    return <ErrorMessage message={chartIndexError.message} />;
+
+  if (!chartIndexData) return <Loader active />;
+
+  if (chartIndexData.charts.length === 0)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Typography>
+          <>現在レンタル中のコーデはありません。</>
+        </Typography>
+      </div>
+    );
+
+  return (
+    <>
+      {selectedChart ? (
+        <>a</>
+      ) : (
+        <Page>
+          <ChartList
+            chartResponses={chartIndexData.charts}
+            onClickChart={setSelectedChart}
+          />
+        </Page>
+      )}
+    </>
+  );
 };
