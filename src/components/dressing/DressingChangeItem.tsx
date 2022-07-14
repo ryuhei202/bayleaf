53b768bf9cc1eaf7e6d624bb172e39d859f442ab
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-07-14 13:09:14 +0900

    feat: 着こなしページに表示するチェンジアイテムコンポーネントを実装 <feature/2743/LEEAP-2841-着こなしページfront-dressingchange>

---
+++ src/components/dressing/DressingChangeItem.tsx
@@ -0,0 +1,29 @@
+import { TDressingItem } from "../../api/dressings/TDressingItem";
+import { Typography } from "../baseParts/Typography";
+import { CoordinateItemImages } from "../review/CoordinateItemImages";
+
+type TProps = {
+  readonly changeItems: TDressingItem[];
+};
+export const DressingChangeItem = ({ changeItems }: TProps) => {
+  const items = changeItems.map((item) => {
+    return {
+      imagePaths: {
+        defaultPath: item.imagePaths.largeThumb,
+        expandedPath: item.imagePaths.large,
+      },
+      caption: `${item.cateSmallName} / ${item.color}`,
+    };
+  });
+  return (
+    <div>
+      <Typography size="sm">チェンジアイテム</Typography>
+      <Typography size="xs" color="strong-gray" className="mt-2 ml-2">
+        ※利用したいシーンに合うアイテムでありながら、別のコーデとして楽しめるアイテムです！
+      </Typography>
+      <div className="my-5">
+        <CoordinateItemImages items={items} />
+      </div>
+    </div>
+  );
+};
