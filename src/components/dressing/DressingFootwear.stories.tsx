d58057b8139a092d4910d40928cb19c400c247e3
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-07-14 12:53:43 +0900

    feat: 着こなしページの靴コンポーネントを実装 <feature/2743/LEEAP-2840-着こなしページfront-dressingfootwear>

---
+++ src/components/dressing/DressingFootwear.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { DressingFootwear } from "./DressingFootwear";
+
+export default {
+  title: "Dressing/DressingFootwear",
+  component: DressingFootwear,
+} as ComponentMeta<typeof DressingFootwear>;
+
+const Template: ComponentStory<typeof DressingFootwear> = (args) => (
+  <DressingFootwear {...args} />
+);
+
+export const DefaultValues = Template.bind({});
+DefaultValues.decorators = [
+  (Story) => (
+    <div className="bg-slate-200 w-full p-3">
+      <Story />
+    </div>
+  ),
+];
+DefaultValues.args = {
+  footwear: {
+    name: "ネイビーのキャンバススニーカー",
+    imageFileName: "/images/favorite_shoes/1.jpg",
+  },
+};
