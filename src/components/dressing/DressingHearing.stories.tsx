f9801baf26f63e8677f87e335032ef963c98f6a5
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-07-14 10:34:11 +0900

    feat: 着こなし相談ページに表示するヒアリングコンポーネントの実装 <feature/2743/LEEAP-2836-着こなしページfront-dressinghearing>

---
+++ src/components/dressing/DressingHearing.stories.tsx
@@ -0,0 +1,83 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { TCategorizedForm } from "../../api/hearings/TCategorizedForm";
+import { TForm } from "../../api/hearings/TForm";
+import { DressingHearing } from "./DressingHearing";
+
+export default {
+  title: "Dressing/DressingHearing",
+  component: DressingHearing,
+} as ComponentMeta<typeof DressingHearing>;
+
+const Template: ComponentStory<typeof DressingHearing> = (args) => (
+  <DressingHearing {...args} />
+);
+
+export const Default = Template.bind({});
+const forms: TForm[] = [
+  {
+    title: "借りたコーデをどちらの予定で使いますか？",
+    options: [
+      {
+        name: "日常の予定",
+        text: null,
+      },
+    ],
+  },
+  {
+    title: "借りたコーデをどちらの予定で使いますか？",
+    options: [
+      {
+        name: "日常の予定",
+        text: null,
+      },
+    ],
+  },
+];
+const hearings = [
+  {
+    categoryId: 1,
+    categoryName: "使いたいシーン",
+    forms: [
+      {
+        title: "借りたコーデをどちらの予定で使いますか？",
+        options: [
+          {
+            name: "日常の予定",
+            text: null,
+          },
+        ],
+      },
+      {
+        title: "どちらの場所で仕事をする時間が長いですか？",
+        options: [
+          {
+            name: "自宅が長い",
+            text: null,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    categoryId: 1,
+    categoryName: "意識する相手",
+    forms: [
+      {
+        title: "普段のお仕事先で会う人を教えてください(オンライン含む)",
+        options: [
+          {
+            name: "同僚(上司、部下)",
+            text: null,
+          },
+          {
+            name: "お客様、取引先",
+            text: null,
+          },
+        ],
+      },
+    ],
+  },
+];
+Default.args = {
+  hearings,
+};
