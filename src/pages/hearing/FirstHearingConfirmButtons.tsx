c8f4a8ff4b214ed0c61f473ee22244ea493efeef
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-08-16 16:05:38 +0900

    feat: ブラウザに依存する確認画面を使用せずheadlessuiのDialogを使用するように変更 <bugfix/LEEAP-2844-ヒアリング確認画面にurlが出てくる>

--- src/pages/hearing/FirstHearingConfirmButtons.tsx
+++ src/pages/hearing/FirstHearingConfirmButtons.tsx
@@ -1,3 +1,5 @@
+import { Dialog } from "@headlessui/react";
+import { useState } from "react";
 import { Button } from "../../components/baseParts/Button";
 
 type TProps = {
@@ -12,6 +14,7 @@ export const FirstHearingConfirmButtons = ({
   onClickReset,
   isPostLoading,
 }: TProps) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <Button
@@ -33,15 +36,34 @@ export const FirstHearingConfirmButtons = ({
       </Button>
       <Button
         variant="text"
-        onClick={() => {
-          if (window.confirm("最初からやり直しますか？")) {
-            onClickReset();
-          }
-        }}
+        onClick={() => setIsOpen(true)}
         disabled={isPostLoading}
       >
         最初からやり直す
       </Button>
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="fixed inset-0 h-screen w-screen bg-black/50"
+      >
+        <Dialog.Panel className="fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-white w-[80vw] p-10 rounded-2xl max-w-[400px]">
+          <Dialog.Title className="pb-5 text-xl mb-5">最初からやり直しますか？</Dialog.Title>
+          <div className="space-y-3">
+            <Button
+              variant="primary"
+              onClick={() => {
+                setIsOpen(false);
+                onClickReset();
+              }}
+            >
+              OK
+            </Button>
+            <Button variant="default" onClick={() => setIsOpen(false)}>
+              キャンセル
+            </Button>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </>
   );
 };
