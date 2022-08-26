5d1d8362f3d0278917d3c8a2c172cc66520e2f1e
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-08-26 12:41:52 +0900

    feat: 着こなし靴画像を拡大できるように修正 <release/LEEAP-3098-着こなしページ靴の拡大表示できる>

--- src/components/dressing/DressingFootwear.tsx
+++ src/components/dressing/DressingFootwear.tsx
@@ -1,4 +1,5 @@
 import { TDressingFootwear } from "../../api/dressings/TDressingFootwear";
+import { ExpandableImage } from "../baseParts/images/ExpandableImage";
 import { Typography } from "../baseParts/Typography";
 
 type TProps = {
@@ -10,10 +11,14 @@ export const DressingFootwear = ({ footwear }: TProps) => {
     <div className="mb-16">
       <Typography size="xl">コーデに合う靴</Typography>
       <div className="flex items-center mt-5">
-        <img
-          src={`${process.env.REACT_APP_HOST_URL + footwear.imagePath}`}
-          alt=""
-          className="w-[20vw] rounded-md"
+        <ExpandableImage
+          defaultImageSrc={`${
+            process.env.REACT_APP_HOST_URL + footwear.imagePath
+          }`}
+          ExpandedImageSrc={`${
+            process.env.REACT_APP_HOST_URL + footwear.imagePath
+          }`}
+          className="w-[30vw] rounded-md"
         />
         <Typography className="px-6">{footwear.name}</Typography>
       </div>
