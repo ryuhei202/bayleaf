0ff0690442f14b03e908841995c0978d6dad2e58
Onishi_Kengo <k-ounishi@kiizan-kiizan.co.jp>
2022-11-15 16:32:22 +0900

    refactor:chartフォルダの移動

--- src/components/resourceParts/chart/ChartList.stories.tsx
+++ src/components/resourceParts/chart/ChartList.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { TChartResponse } from "../../api/charts/TChartResponse";
+import { TChartResponse } from "../../../api/charts/TChartResponse";
 import { ChartList } from "./ChartList";
 
 export default {
--- src/pages/consult/Consult.tsx
+++ src/pages/consult/Consult.tsx
@@ -5,7 +5,7 @@ import { TChartResponse } from "../../api/charts/TChartResponse";
 import { useChartIndex } from "../../api/charts/useChartIndex";
 import { Page } from "../../components/baseParts/Page";
 import { Typography } from "../../components/baseParts/Typography";
-import { ChartList } from "../../components/chart/ChartList";
+import { ChartList } from "../../components/resourceParts/chart/ChartList";
 import { ErrorMessage } from "../../components/shared/ErrorMessage";
 import { CHART_RENTAL_STATUS } from "../../models/chart/ChartRentalStatus";
 import { ConsultFetcher } from "./ConsultFetcher";
