000ad7c0a142fd9c754010f35dbd80c0afd4ba81
Onishi_Kengo <k-ounishi@kiizan-kiizan.co.jp>
2022-11-15 14:46:30 +0900

    fix:hooks名の修正

--- src/api/simplifiedHearings/useSimplifiedHearingShow.ts
+++ src/api/simplifiedHearings/useSimplifiedHearingShow.ts
@@ -1,6 +1,6 @@
 import { useGetRequest } from "../useGetRequest";
 import { TSimplifiedHearingShowResponse } from "./TSimplifiedHearingShowResponse";
-type TSimplifiedHearingDate = {
+type TSimplifiedHearing = {
   readonly data?: TSimplifiedHearingShowResponse;
   readonly error: Error | null;
 };
@@ -9,9 +9,9 @@ type TSimplifiedHearingParams = {
   coordinateId?: number;
 };
 
-export const useDeliveryDateShow = ({
+export const useSimplifiedHearingShow = ({
   coordinateId,
-}: TSimplifiedHearingParams): TSimplifiedHearingDate => {
+}: TSimplifiedHearingParams): TSimplifiedHearing => {
   const { data, error } = useGetRequest<TSimplifiedHearingShowResponse>(
     `coordinates/${coordinateId}/simplified_hearings`
   );
