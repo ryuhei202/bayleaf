1f0e69542d5ff66a8aa99f68d163af80a8ecedc6
Onishi_Kengo <k-ounishi@kiizan-kiizan.co.jp>
2022-11-15 17:52:51 +0900

    feat:useSimplifiedHearingShowによるデータの取得

--- src/pages/review/ReviewForm.tsx
+++ src/pages/review/ReviewForm.tsx
@@ -1,10 +1,13 @@
 import { TCoordinateResponse } from "../../api/coordinates/TCoordinateResponse";
 import { TReviewOptionResponse } from "../../api/reviews/TReviewOptionResponse";
+import { useSimplifiedHearingShow } from "../../api/simplifiedHearings/useSimplifiedHearingShow";
 import { Button } from "../../components/baseParts/Button";
 import { CoordinateItemImages } from "../../components/baseParts/CoordinateItemImages";
 import { Page } from "../../components/baseParts/Page";
 import { PageHeader } from "../../components/baseParts/PageHeader";
 import { convertItemsToItemImagesProps } from "../../components/pageParts/review/convertItemsToItemImagesProps";
+import { SimpifiedHearing } from "../../components/resourceParts/simplifiedHearing/SimpifiedHearing";
+import { ErrorMessage } from "../../components/shared/ErrorMessage";
 
 type Props = {
   readonly coordinate: TCoordinateResponse;
@@ -13,6 +16,13 @@ type Props = {
 };
 
 export const ReviewForm = ({ coordinate, reviewOptions, onSubmit }: Props) => {
+  const { data: simplifiedHearing, error: simplifiedHearingError } =
+    useSimplifiedHearingShow({
+      coordinateId: coordinate.id,
+    });
+
+  if (simplifiedHearingError)
+    return <ErrorMessage message={simplifiedHearingError.message} />;
   return (
     <Page>
       <div className="flex flex-col justify-between h-full">
@@ -29,6 +39,12 @@ export const ReviewForm = ({ coordinate, reviewOptions, onSubmit }: Props) => {
             {...convertItemsToItemImagesProps(coordinate.items)}
           />
         </div>
+        <SimpifiedHearing
+          title="今回のコーデ情報"
+          target={simplifiedHearing?.target as string}
+          scene={simplifiedHearing?.scene as string}
+          impression={simplifiedHearing?.impression as string}
+        />
         <div className="flex flex-col space-y-3 bg-white p-5">
           {reviewOptions.map((option) => (
             <Button
