3862092e31c820266429d2afafe8c19a64ce3aee
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-11-24 10:42:42 +0900

    wip: Jestの設定ファイルでCommonJS対応をする <feature/3486/LEEAP-3537-初回プラン変更フロントテスト実装>

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
 const config = {
   testEnvironment: "jsdom",
-
+  // moduleDirectories: ["node_modules", "<rootDir>/"],
   testMatch: ["**/*.test.js", "**/*.test.ts", "**/*.test.tsx"],
-
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
   testPathIgnorePatterns: ["<rootDir>/node_modules/"],
-
+  transformIgnorePatterns: [
+    "node_modules/(?![@line|promise-polyfill|jest-cli]).+\\.js$",
+    "node_modules/(?![@line|promise-polyfill|jest-cli]).+\\.ts$",
+    "node_modules/(?![@line|promise-polyfill|jest-cli]).+\\.tsx$",
+  ],
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/mocks/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/src/mocks/styleMock.js",
+  },
   transform: {
     ".+\\.tsx?$": [
       "@swc/jest",
---
+++ src/__tests__/pages/delivery/Delivery.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { Dressing } from "../../../pages/dressing/Dressing";
+
+test("テストのテスト", () => {
+  const { container, debug } = render(<Dressing />);
+  // expect(container.)
+  debug();
+});
