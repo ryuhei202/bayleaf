794a98b2c9dc8251d4118334e1ded15937d89d98
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-12-19 12:18:16 +0900

    feat: ダイアログは開いたり削除したりするタイミングが多いのでmemo化する <feature/3621/LEEAP-3641-プラン変更bayleaf-planselecting.ts>

--- src/components/baseParts/dialogs/ConfirmDialog.tsx
+++ src/components/baseParts/dialogs/ConfirmDialog.tsx
@@ -1,4 +1,5 @@
 import { Dialog } from "@headlessui/react";
+import React from "react";
 import { Button } from "../Button";
 
 type TProps = {
@@ -12,47 +13,50 @@ type TProps = {
   readonly onClickCancel: () => void;
   readonly onClose: () => void;
 };
-export const ConfirmDialog = ({
-  open,
-  title,
-  description,
-  okBtnText,
-  cancelBtnText,
-  isLoading,
-  onClickOk,
-  onClickCancel,
-  onClose,
-}: TProps) => {
-  return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      className="fixed inset-0 h-screen w-screen bg-black/50 z-20"
-    >
-      <Dialog.Panel className="fixed bottom-0 left-1/2 translate-x-[-50%] bg-[#F6F6F6] w-screen px-10 py-6 rounded-t-2xl text-themeGray text-center">
-        <Dialog.Title className="pb-5 font-bold">{title}</Dialog.Title>
-        <Dialog.Description className="mb-7 text-xs">
-          {description}
-        </Dialog.Description>
-        <div className="flex mb-7">
-          <Button
-            variant="line"
-            onClick={onClickCancel}
-            isLoading={isLoading}
-            className="mx-2 w-[70%]"
-          >
-            {cancelBtnText ?? "キャンセル"}
-          </Button>
-          <Button
-            variant="default"
-            onClick={onClickOk}
-            disabled={isLoading}
-            className="mx-2"
-          >
-            {okBtnText ?? "OK"}
-          </Button>
-        </div>
-      </Dialog.Panel>
-    </Dialog>
-  );
-};
+
+export const ConfirmDialog = React.memo(
+  ({
+    open,
+    title,
+    description,
+    okBtnText,
+    cancelBtnText,
+    isLoading,
+    onClickOk,
+    onClickCancel,
+    onClose,
+  }: TProps) => {
+    return (
+      <Dialog
+        open={open}
+        onClose={onClose}
+        className="fixed inset-0 h-screen w-screen bg-black/50 z-20"
+      >
+        <Dialog.Panel className="fixed bottom-0 left-1/2 translate-x-[-50%] bg-[#F6F6F6] w-screen px-10 py-6 rounded-t-2xl text-themeGray text-center">
+          <Dialog.Title className="pb-5 font-bold">{title}</Dialog.Title>
+          <Dialog.Description className="mb-7 text-xs">
+            {description}
+          </Dialog.Description>
+          <div className="flex mb-7">
+            <Button
+              variant="line"
+              onClick={onClickCancel}
+              isLoading={isLoading}
+              className="mx-2 w-[70%]"
+            >
+              {cancelBtnText ?? "キャンセル"}
+            </Button>
+            <Button
+              variant="default"
+              onClick={onClickOk}
+              disabled={isLoading}
+              className="mx-2"
+            >
+              {okBtnText ?? "OK"}
+            </Button>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    );
+  }
+);
