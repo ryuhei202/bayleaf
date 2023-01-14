6e2ce63aa35fd7a90fb6ad56c401129546dacd1a
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-01-14 17:33:54 +0900

    feat: CancelActionTextとstoriesを作成。 <feature/3462/LEEAP-3669-プランキャンセル-planselecting.tsxの修正>

---
+++ src/components/baseParts/text/CancelActionText.tsx
@@ -0,0 +1,17 @@
+import ErrorIcon from "../../../images/icons/error.svg";
+
+type TProps = {
+  readonly children: string;
+  readonly onClick: () => void;
+};
+
+export const CancelActionText = ({ children, onClick }: TProps) => {
+  return (
+    <button className="text-red" onClick={onClick}>
+      <img className="inline w-5 h-5" src={ErrorIcon}></img>
+      <span className="underline underline-offset-2 decoration-0 font-thin text-sm align-middle ">
+        {children}
+      </span>
+    </button>
+  );
+};
