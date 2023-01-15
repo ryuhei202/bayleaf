374051f7269774062e525ea3ced65406e6a27198
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-01-15 13:03:30 +0900

    feat: 変更予約取り消し処理の追加。変更予約取り消しの完了ダイアログの作成。 <feature/LEEAP-3670-プランキャンセル-planselectingcontain>

--- src/pages/planChange/PlanSelectingContainer.tsx
+++ src/pages/planChange/PlanSelectingContainer.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "react";
 import { TMembersIndexResponse } from "../../api/members/TMembersIndexResponse";
 import { useMembersPlanChange } from "../../api/members/useMembersPlanChange";
+import { usePlanRequestsDestroy } from "../../api/planRequests/usePlanRequestsDestroy";
 import { PlanSelecting } from "../../components/pageParts/planChange/PlanSelecting";
 import { findPlanById, TPlan } from "../../models/shared/Plans";
 
@@ -12,13 +13,19 @@ export const PlanSelectingContainer = ({ memberData }: TProps) => {
   const [selectedPlan, setSelectedPlan] = useState<TPlan>();
   const [isNextPayment, setIsNextPayment] = useState<boolean>(true);
   const [isCompleted, setIsCompleted] = useState<boolean>(false);
-  const { mutate, isLoading } = useMembersPlanChange({
-    memberId: memberData.id,
-  });
+  const [isCancelCompleted, setIsCancelCompleted] = useState<boolean>(false);
+  const { mutate: planChangeMutate, isLoading: isPlanChangeLoading } =
+    useMembersPlanChange({
+      memberId: memberData.id,
+    });
+  const { mutate: requestDestroyMutate, isLoading: isRequestDestroyLoading } =
+    usePlanRequestsDestroy({
+      memberId: memberData.id,
+    });
 
   const handleSubmit = () => {
     if (!selectedPlan) return;
-    mutate(
+    planChangeMutate(
       { planId: selectedPlan.id, isNextPayment },
       {
         onSuccess: () => {
@@ -26,6 +33,14 @@ export const PlanSelectingContainer = ({ memberData }: TProps) => {
         },
       }
     );
+  };
+
+  const handleCancelPlanChange = () => {
+    requestDestroyMutate(undefined, {
+      onSuccess: () => {
+        setIsCancelCompleted(true);
+      },
+    });
   };
 
   const handleCancel = useCallback(() => {
@@ -42,16 +57,27 @@ export const PlanSelectingContainer = ({ memberData }: TProps) => {
   };
 
   return (
-    <PlanSelecting
-      memberData={memberData}
-      selectedPlan={selectedPlan}
-      isLoading={isLoading}
-      isNextPayment={isNextPayment}
-      isCompleted={isCompleted}
-      onSubmit={handleSubmit}
-      onCancel={handleCancel}
-      onPlanSelect={handlePlanSelect}
-      onTimingChange={handleTimingChange}
-    />
+    <>
+      <PlanSelecting
+        memberData={{
+          mPlanId: memberData.mPlanId,
+          nextPaymentDate: memberData.nextPaymentDate,
+          rentalRemainingNum: memberData.rentalRemainingNum,
+          // requestedPlanId : memberData.requestedPlanId === null ? undefined: memberData.requestedPlanId
+          requestedPlanId: memberData.requestedPlanId ?? undefined,
+        }}
+        selectedPlan={selectedPlan}
+        isPlanChangeLoading={isPlanChangeLoading}
+        isRequestDestroyLoading={isRequestDestroyLoading}
+        isNextPayment={isNextPayment}
+        isCompleted={isCompleted}
+        isCancelCompleted={isCancelCompleted}
+        onSubmit={handleSubmit}
+        onCancel={handleCancel}
+        onPlanSelect={handlePlanSelect}
+        onTimingChange={handleTimingChange}
+        onCancelPlanChange={handleCancelPlanChange}
+      />
+    </>
   );
 };
