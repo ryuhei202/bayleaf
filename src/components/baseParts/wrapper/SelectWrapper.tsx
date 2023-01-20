336d5c2ab11206f6a7275434504a9abdcd7ea477
Onishi_Kengo <k-ounishi@kiizan-kiizan.co.jp>
2023-01-20 10:05:03 +0900

    fix:select時のチェックを可変から固定へ <release/LEEAP-3699-bayleafresourceparts-itemcard>

--- src/components/baseParts/wrapper/SelectWrapper.tsx
+++ src/components/baseParts/wrapper/SelectWrapper.tsx
@@ -12,7 +12,7 @@ export const SelectWrapper = ({ visible, children }: TProps) => {
         <div className="border-2 border-themeGray relative rounded-md">
           <span
             className="text-center absolute -top-3 left-1/2 text-white 
-          bg-themeGray w-1/4 h-6 flex justify-center items-center rounded-full -translate-x-1/2 "
+          bg-themeGray w-6 h-6 flex justify-center items-center rounded-full -translate-x-1/2 "
           >
             ✔︎
           </span>
