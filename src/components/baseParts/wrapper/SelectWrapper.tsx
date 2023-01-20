08c87dc4f14be9e34ac6de7d3920a819c9eab3af
Onishi_Kengo <k-ounishi@kiizan-kiizan.co.jp>
2023-01-20 12:41:46 +0900

    fiix:未選択時のUIを変更 <release/LEEAP-3699-bayleafresourceparts-itemcard>

--- src/components/baseParts/wrapper/SelectWrapper.tsx
+++ src/components/baseParts/wrapper/SelectWrapper.tsx
@@ -19,7 +19,13 @@ export const SelectWrapper = ({ visible, children }: TProps) => {
           {children}
         </div>
       ) : (
-        <div className="border-2 border-transparent relative rounded-md">
+        <div className="border-2 border-slate-300 relative rounded-md">
+          <span
+            className="text-center absolute -top-3 left-1/2 text-white 
+          bg-slate-300 w-6 h-6 flex justify-center items-center rounded-full -translate-x-1/2 "
+          >
+            ✔︎
+          </span>
           {children}
         </div>
       )}
