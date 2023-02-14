48683b8f3da6e9b6021d60bcd472f16303d5e9c1
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2023-02-14 16:42:08 +0900

    style: iosで表示崩れしている問題を修正 <bugfix/LEEAP-3874-bayleafiosでモーダル表記が崩れる>

--- src/components/baseParts/dialogs/ConfirmDialog.tsx
+++ src/components/baseParts/dialogs/ConfirmDialog.tsx
@@ -29,29 +29,29 @@ export const ConfirmDialog = ({
       onClose={onClose}
       className="fixed inset-0 h-screen w-screen bg-black/50 z-20"
     >
-      <Dialog.Panel className="fixed h-fit bottom-0 left-1/2 translate-x-[-50%] bg-[#F6F6F6] w-screen px-10 py-6 rounded-t-2xl text-themeGray text-center">
+      <Dialog.Panel className="fixed h-fit bottom-0 left-1/2 translate-x-[-50%] bg-[#F6F6F6] w-screen px-10 py-6 rounded-t-2xl text-themeGray text-center pb-7">
         <Dialog.Title className="pb-5 font-bold">{title}</Dialog.Title>
-        <Dialog.Description className="h-full mb-7 text-xs">
-          {description}
+        <Dialog.Description className="h-full text-xs" as="div">
+          <div className="mb-7">{description}</div>
+          <div className="flex">
+            <Button
+              variant="line"
+              onClick={onClickCancel}
+              isLoading={isLoading}
+              className="mx-2 w-[70%]"
+            >
+              {cancelBtnText ?? "キャンセル"}
+            </Button>
+            <Button
+              variant="default"
+              onClick={onClickOk}
+              disabled={isLoading}
+              className="mx-2"
+            >
+              {okBtnText ?? "OK"}
+            </Button>
+          </div>
         </Dialog.Description>
-        <div className="flex mb-7">
-          <Button
-            variant="line"
-            onClick={onClickCancel}
-            isLoading={isLoading}
-            className="mx-2 w-[70%]"
-          >
-            {cancelBtnText ?? "キャンセル"}
-          </Button>
-          <Button
-            variant="default"
-            onClick={onClickOk}
-            disabled={isLoading}
-            className="mx-2"
-          >
-            {okBtnText ?? "OK"}
-          </Button>
-        </div>
       </Dialog.Panel>
     </Dialog>
   );
