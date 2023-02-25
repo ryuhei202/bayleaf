45e9f1474b22753b2c31cb449977311bc37c87cd
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-02-25 18:28:55 +0900

    wip: 領収書のcssを適用 <feature/3806/LEEAP-3903-決済履歴bayleafreceipt.tsxの実装>

--- src/pages/paymentHistory/Receipt.tsx
+++ src/pages/paymentHistory/Receipt.tsx
@@ -1,12 +1,19 @@
 import { Document, Page, StyleSheet } from "@react-pdf/renderer";
 
+type TReceiptDetails = {
+  title: string;
+  priceTaxIn: number;
+}[];
+
 type TProps = {
-  receiptDetails: {
-    title: string;
-    priceTaxIn: number;
-  }[];
-  cardBrand: String;
-  cardNumber: String;
+  //追加しなければいけないProps
+  memberPaymentId: number;
+  receiptCreatedAt: string;
+  useingPoint: number;
+  //ここまで
+  receiptDetails: TReceiptDetails;
+  cardBrand: string;
+  cardNumber: string;
 };
 
 // Create styles
@@ -23,119 +30,129 @@ const styles = StyleSheet.create({
 });
 
 // Create Document Component
-export const Receipts = ({ receiptDetails, cardBrand, cardNumber }: TProps) => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth() + 1;
-  const day = today.getDate();
+export const Receipts = ({
+  memberPaymentId,
+  receiptCreatedAt,
+  receiptDetails,
+  cardBrand,
+  cardNumber,
+  useingPoint,
+}: TProps) => {
+  const date = new Date(receiptCreatedAt);
 
-  const formattedDate =
-    year + "年" + ("0" + month).slice(-2) + "月" + ("0" + day).slice(-2) + "日";
-  console.log(formattedDate);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  const formattedDate = `${year}年${month}月${day}日`;
+
+  const getTotalPrice = (receiptDetails: TReceiptDetails) => {
+    let totalPrice = 0;
+    receiptDetails.map((receipt) => {
+      totalPrice += receipt.priceTaxIn;
+    });
+    return totalPrice;
+  };
+
   return (
     <Document>
       <Page size="A4" style={styles.page}>
-        <div>
-          <div className="header txt-right float-l">
-            発行日 : <span>{formattedDate}</span>
+        <div className="text-[1px] flex flex-col text-center w-90">
+          {/* div1 */}
+          <div>
+            <div className="">
+              発行日 : <span>{formattedDate}</span>
+            </div>
+            <h2 className="text-[14px] font-bold">{`領収書　(注文番号 :${memberPaymentId})`}</h2>
+            <div className="underline text-[#428bca]">
+              印刷してご利用ください
+            </div>
           </div>
-        </div>
-        <br />
-        <h2>領収書　(注文番号 :)</h2>
-        {/* <%= @t_member_payment.payment_id %> */}
-        <div className="text-[10px] underline text-[#428bca]">
-          印刷してご利用ください
-        </div>
-        <div className="w-4/5 mx-auto mt-[40px] mb-0"></div>
-        <div className="header float-l txt-left">
-          <table>
-            <span className="bold">注文日　 : </span>
+          {/* div2 */}
+          <div className="">
+            <div className="w-1/2 float-left text-left">
+              <table>
+                <span className="font-bold">注文日 </span>
+                <span>: {formattedDate} </span>
+                <br />
+                <span className="font-bold">注文番号 </span>
+                <span> : {memberPaymentId} </span>
+                <br />
+                <span className="font-bold">
+                  ご請求額 : ¥
+                  {(
+                    getTotalPrice(receiptDetails) - useingPoint
+                  ).toLocaleString()}
+                </span>
+              </table>
+            </div>
+            <div className="w-1/2 float-left font-bold text-right underline">
+              　　　　　　　　　　　　様
+            </div>
+          </div>
+          <br />
+          {/* div3 */}
+          <div className="">
+            <h2 className="text-[12px] font-bold mb-1">支払い内容</h2>
+            <div>
+              <div className="float-left w-3/5 text-left">
+                <span className="font-bold">注文商品</span>
+              </div>
+              <div className="float-left w-2/5 text-right">
+                <span className="font-bold">価格</span>
+              </div>
+            </div>
+            <div>
+              {receiptDetails.map((receipt, index) => {
+                return (
+                  <div className="" key={index}>
+                    <div className="float-left w-3/5 text-left mb-2">
+                      {receipt.title}
+                    </div>
+                    <div className="float-left w-2/5 text-right">
+                      ¥{receipt.priceTaxIn.toLocaleString()}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          {/* div4 */}
+          <div>
+            <h2 className="text-[12px] font-bold mb-1">支払い情報</h2>
+            <div className="float-left text-left">
+              <span className="font-bold">支払い方法 (クレジットカード)</span>
+            </div>
             <br />
-            <span className="bold">注文番号 : </span>
-            <br />
-            <span className="bold">ご請求額 : ¥{}</span>
-          </table>
-        </div>
-        <div className="w-1/2 float-left bold text-right underline">
-          　　　　　　　　　　様
-        </div>
-        <br />
-        <h3>支払い内容</h3>
-        <div>
-          <div className="float-left w-3/5 text-left text-xs">
-            <span className="bold">注文商品</span>
+
+            <div>
+              <div className="float-left w-3/5 text-left">
+                {cardBrand ?? "-"} | {cardNumber ?? "-"}
+              </div>
+              <div className="float-left w-2/5 text-right">
+                {/* 合計を計算する関数を書きまーす */}
+                商品の小計 : ¥{getTotalPrice(receiptDetails).toLocaleString()}
+                <br />
+                -----
+                <br />
+                {/* ご利用ぽいんとがpropsに必要ですねーい */}
+                ご利用ポイント : {useingPoint}
+                <br /> -----
+                <br />
+                <span className="font-bold">
+                  {/* 商品の小計 - ご利用ポイント */}
+                  ご請求額 : ¥
+                  {(
+                    getTotalPrice(receiptDetails) - useingPoint
+                  ).toLocaleString()}
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="float-left w-2/5 text-right text-xs">
-            <span className="bold">価格</span>
-          </div>
+          {/* div5 */}
+          <div className="mt-[30px] text-[10px]">© {year} Uwear</div>
         </div>
-        <br />
-        {receiptDetails.forEach((receipt) => {})}
-        {/* <% @receipt_details.each do | detail | %>
-    <div>
-      <div className="detail-list-1"><%= detail.title %></div>
-      <div className="detail-list-2">¥ <%= number_with_delimiter(detail.price_tax_in) %></div>
-    </div><br clear="all" />
-    <% end %> */}
       </Page>
     </Document>
   );
 };
-
-/* <div className="container">
-  <div>
-    <div className="header txt-right float-l">発行日 : <span>Date.today.strftime("%Y年%m月%d日")</span></div>
-  </div>
-  <br clear="all" />
-  <h2>領収書　(注文番号 : <%= @t_member_payment.payment_id %>)</h2>
-  <div className="note link">印刷してご利用ください</div>
-  <div className="content">
-    <div className="header float-l txt-left">
-      <span className="bold">注文日　 : </span><%= @t_member_payment.created_at.strftime("%Y年%m月%d日") %><br>
-      <span className="bold">注文番号 : </span><%= @t_member_payment.payment_id %><br>
-      <span className="bold">ご請求額 : ¥ <%= number_with_delimiter(@t_member_payment.price_tax_in) %></span>
-      </table>
-    </div>
-    <div className="name float-l bold txt-right underline">　　　　　　　　　　　　　　様</div>
-    <br clear="all" />
-
-    <h3>支払い内容</h3>
-    <div>
-      <div className="detail-list-1"><span className="bold">注文商品</span></div>
-      <div className="detail-list-2"><span className="bold">価格</span></div>
-    </div><br clear="all" />
-    <% @receipt_details.each do | detail | %>
-    <div>
-      <div className="detail-list-1"><%= detail.title %></div>
-      <div className="detail-list-2">¥ <%= number_with_delimiter(detail.price_tax_in) %></div>
-    </div><br clear="all" />
-    <% end %>
-
-    <h3>支払い情報</h3>
-    <div>
-    <div className="detail-list-0"><span className="bold">支払い方法&nbsp;(クレジットカード）</span></div>
-    </div><br clear="all" />
-    <div>
-      <div className="detail-list-1"><%= @paygent_customer["card_brand"] %> | <%= @paygent_customer["card_number"] %></div>
-      <div className="detail-list-2">
-        商品の小計 : ¥ <%= number_with_delimiter(@total_price) %><br>
-        -----<br>
-        ご利用ポイント : <%= number_with_delimiter(@t_member_payment.point) %>pt<br>
-        -----<br>
-        <span className="bold">ご請求額 : ¥ <%= number_with_delimiter(@t_member_payment.price_tax_in) %></span><br>
-      </div>
-    </div><br clear="all" />
-<%
-=begin
-%>
-    <div className="mt20">
-      <div className="detail-list-3"><span className="bold">クレジットカードへの請求</span></div>
-      <div className="detail-list-4"><%= @paygent_customer["card_brand"] %>(下4桁が<%= @paygent_customer["card_number"].delete("*") %>):<%= @t_member_payment.created_at.strftime("%Y年%m月%d日") %>:¥<%= number_with_delimiter(@t_member_payment.price_tax_in) %></div>
-    </div><br clear="all" />
-<%
-=end
-%>
-
-    <div className="footer">© 2022 <%= SERVICE_NAME %></div>
-  </div>
-</div> */
--- src/pages/paymentHistory/Receipts.stories.tsx
+++ src/pages/paymentHistory/Receipts.stories.tsx
@@ -6,14 +6,32 @@ export default {
   component: Receipts,
   decorators: [
     (Story) => (
-      <div className="m-1 p-2 bg-clay">
+      <div className="m-1 p-2 bg-white">
         <Story />
       </div>
     ),
   ],
 } as ComponentMeta<typeof Receipts>;
 
-const Template: ComponentStory<typeof Receipts> = (args) => <Receipts />;
+const Template: ComponentStory<typeof Receipts> = (args) => (
+  <Receipts {...args} />
+);
 
 export const DefaultValues = Template.bind({});
-DefaultValues.args = {};
+DefaultValues.args = {
+  receiptCreatedAt: "2022-02-25",
+  memberPaymentId: 12344,
+  useingPoint: 3000,
+  receiptDetails: [
+    {
+      title: "Levi'sジーンズ",
+      priceTaxIn: 8800,
+    },
+    {
+      title: "FORK&SPOON柄カットソー",
+      priceTaxIn: 8800,
+    },
+  ],
+  cardBrand: "VISA",
+  cardNumber: "1111",
+};
