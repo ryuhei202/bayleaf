ac72f3c3eef44eed9a42abdb50b85a28f4820acf
東内雅也 <m-tounai@kiizan-kiizan.co.jp>
2023-02-27 18:19:27 +0900

    feat:型を共通化、領収書が発行できない決済履歴はボタンを表示しない <feature/LEEAP-3905-決済履歴bayleafpaymenthistorylistitemを実装>

--- src/components/pageParts/paymentHistory/PaymentHistoryListItem.stories.tsx
+++ src/components/pageParts/paymentHistory/PaymentHistoryListItem.stories.tsx
@@ -1,6 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { PaymentHistoryListItem } from "./PaymentHistoryListItem";
-
 export default {
   title: "PageParts/paymentHistory/PaymentHistoryListItem",
   component: PaymentHistoryListItem,
@@ -12,15 +11,17 @@ export default {
     ),
   ],
 } as ComponentMeta<typeof PaymentHistoryListItem>;
-
 const Template: ComponentStory<typeof PaymentHistoryListItem> = (args) => (
   <PaymentHistoryListItem {...args} />
 );
-
 export const DefaultValues = Template.bind({});
 DefaultValues.args = {
-  priceTaxIn: 10000,
-  memberPaymentId: 5,
-  paymentDate: "2022年5月24日",
-  paymentTypeName: "月額利用",
+  memberPayment: {
+    paymentId: "weiru",
+    priceTaxIn: 10000,
+    memberPaymentId: 5,
+    paymentDate: "2022年5月24日",
+    paymentTypeName: "月額利用料金",
+    isAvailableReceipt: true,
+  },
 };
--- src/components/pageParts/paymentHistory/PaymentHistoryListItem.tsx
+++ src/components/pageParts/paymentHistory/PaymentHistoryListItem.tsx
@@ -1,49 +1,47 @@
 import { Button } from "../../baseParts/Button";
 import { Typography } from "../../baseParts/legacy/Typography";
+import { TMemberPayments } from "./PaymentHistoryList";
 
 type TProps = {
-  priceTaxIn: number;
-  memberPaymentId: number;
-  paymentDate: string;
-  paymentTypeName: string;
+  memberPayment: TMemberPayments;
   onClickReceiptButton: (memberPaymentId: number) => void;
 };
 
 export const PaymentHistoryListItem = ({
-  priceTaxIn,
-  memberPaymentId,
-  paymentDate,
-  paymentTypeName,
+  memberPayment,
   onClickReceiptButton,
 }: TProps) => {
   return (
-    <div className="bg-white flex justify-center items-center h-12">
-      <div className="mx-2">
+    <div className="bg-white flex justify-self-start items-center h-12 mb-1">
+      <div className="basis-1/4">
         <Typography color="strong-gray" size="xs">
-          {paymentDate}
+          {memberPayment.paymentDate}
         </Typography>
       </div>
-      <div className="mx-2">
+      <div className="basis-1/4">
         <Typography color="strong-gray" size="xs">
-          {paymentTypeName}
+          {memberPayment.paymentTypeName}
         </Typography>
       </div>
-      <div className="mx-2">
+      <div className="basis-1/4">
         <Typography color="strong-gray" size="xs">
-          {priceTaxIn}円
+          {memberPayment.priceTaxIn.toLocaleString()}円
         </Typography>
       </div>
-      <div className="mx-2">
-        <Button
-          onClick={() => onClickReceiptButton(memberPaymentId)}
-          variant="line"
-          size="small"
-          className=""
-        >
-          <Typography color="strong-gray" size="xs">
-            領収書を発行
-          </Typography>
-        </Button>
+      <div className="basis-1/4">
+        {memberPayment.isAvailableReceipt ? (
+          <Button
+            onClick={() => onClickReceiptButton(memberPayment.memberPaymentId)}
+            variant="line"
+            size="small"
+          >
+            <Typography color="strong-gray" size="xs">
+              領収書を発行
+            </Typography>
+          </Button>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
