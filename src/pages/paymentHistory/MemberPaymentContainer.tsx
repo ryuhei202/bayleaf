721f377f68747dfc65dc6d1ab4ae520040b534ee
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-03-09 15:19:03 +0900

    feat: ReceiotContainerを作成し、領収書関連のデータを取得するコンポーネントとして作成 <feature/3806/LEEAP-3904-決済履歴bayleafmemberpaymentconta>

--- src/pages/paymentHistory/MemberPaymentContainer.tsx
+++ src/pages/paymentHistory/MemberPaymentContainer.tsx
@@ -1,30 +1,23 @@
 import { Dialog } from "@headlessui/react";
-import { useRef, useState } from "react";
-import ReactToPrint from "react-to-print";
+import { useState } from "react";
 import { useMemberPaymentsIndex } from "../../api/memberPayments/useMemberPaymentsIndex";
-import { useReceiptShow } from "../../api/receipts/useReceiptShow";
 import { ErrorPage } from "../../components/baseParts/pages/ErrorPage";
 import { LoaderPage } from "../../components/baseParts/pages/LoaderPage";
 import { MemberPayment } from "./MemberPayment";
-import { Receipts } from "./Receipt";
+import { ReceiptCointainer } from "./ReceiptContainer";
 
 type TProps = {
   nextPaymentDate: string;
 };
 export const MemberPaymentContainer = ({ nextPaymentDate }: TProps) => {
-  const componentRef = useRef(null);
   const [selectedPage, setSelectedPage] = useState<number>(1);
   const [isOpen, setIsOpen] = useState(false);
-  const [memberPaymentId, setMemberPaymentId] = useState<number>(0);
+  const [memberPaymentId, setMemberPaymentId] = useState<number>();
 
   const { data: memberPaymentsData, error: memberPaymentError } =
     useMemberPaymentsIndex({
       params: { limit: 10, offset: (selectedPage - 1) * 10, order: "desc" },
     });
-
-  const { data: receiptData, error: receiptError } = useReceiptShow({
-    memberPaymentId: memberPaymentId,
-  });
 
   const handleClickPagination = (page: number) => {
     setSelectedPage(page);
@@ -38,39 +31,22 @@ export const MemberPaymentContainer = ({ nextPaymentDate }: TProps) => {
   if (memberPaymentError)
     return <ErrorPage message={memberPaymentError.message} />;
   if (!memberPaymentsData) return <LoaderPage />;
-
-  const getPanel = () => {
-    if (receiptError) return <ErrorPage message={receiptError.message} />;
-    if (!receiptData) return <LoaderPage />;
+  if (memberPaymentId)
     return (
-      <div>
-        <ReactToPrint
-          trigger={() => <button>プリントアウト！</button>}
-          content={() => componentRef.current}
-        />
-        <Receipts
-          memberPaymentId={
-            memberPaymentsData.memberPayments[memberPaymentId].paymentId
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+        <Dialog.Panel>
+          {
+            <ReceiptCointainer
+              memberPaymentId={memberPaymentId}
+              memberPaymentsData={memberPaymentsData}
+            />
           }
-          receiptCreatedAt={
-            memberPaymentsData.memberPayments[memberPaymentId].paymentDate
-          }
-          usingPoint={memberPaymentsData.memberPayments[memberPaymentId].point}
-          receiptDetails={receiptData?.receiptDetails}
-          cardBrand={receiptData?.cardBrand}
-          cardNumber={receiptData?.cardNumber}
-          ref={componentRef}
-        />
-      </div>
+        </Dialog.Panel>
+      </Dialog>
     );
-  };
 
   return (
     <>
-      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-        <Dialog.Panel>{getPanel()}</Dialog.Panel>
-      </Dialog>
-
       <MemberPayment
         currentPage={selectedPage}
         onClickPagenation={handleClickPagination}
