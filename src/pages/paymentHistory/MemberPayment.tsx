2e32215de7cd5df9261ad1398dac0a1d367089e9
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-03-15 17:06:59 +0900

    fix: 背景色を画面全体に適用させる <feature/3806/LEEAP-3954-決済履歴bayleafpaymenthistory.tsx>

--- src/pages/paymentHistory/MemberPayment.tsx
+++ src/pages/paymentHistory/MemberPayment.tsx
@@ -1,5 +1,7 @@
+import { Page } from "../../components/baseParts/legacy/Page";
 import { Typography } from "../../components/baseParts/legacy/Typography";
 import { Pagination } from "../../components/baseParts/Pagination";
+
 import {
   PaymentHistoryList,
   TMemberPayment,
@@ -21,42 +23,40 @@ export const MemberPayment = ({
   onClickReceiptButton,
 }: TProps) => {
   return (
-    <>
-      <div className="p-4 bg-clay">
-        <Typography weight="regular" size="xl" color="primary" className="ml-4">
-          決済履歴
+    <Page className="p-4">
+      <Typography weight="regular" size="xl" color="primary" className="ml-4">
+        決済履歴
+      </Typography>
+      <div className="border-[0.5px] border-gray my-2" />
+      <div className="flex">
+        <Typography
+          weight="regular"
+          size="base"
+          color="primary"
+          className="ml-2 mb-4 basis-3/5"
+        >
+          次回決済予定日:
         </Typography>
-        <div className="border-[0.5px] border-gray my-2" />
-        <div className="flex">
-          <Typography
-            weight="regular"
-            size="base"
-            color="primary"
-            className="ml-2 mb-4 basis-3/5"
-          >
-            次回決済予定日:
-          </Typography>
-          <Typography
-            weight="regular"
-            size="base"
-            color="primary"
-            className="ml-2 mb-4  basis-2/5"
-          >
-            {nextPaymentDate}
-          </Typography>
-        </div>
-
-        <PaymentHistoryList
-          memberPayments={paymentData}
-          onClickReceiptButton={onClickReceiptButton}
-        />
-        <Pagination
-          maxPage={maxPage}
-          currentPage={currentPage}
-          onClickPagination={onClickPagenation}
-          className="mt-6"
-        />
+        <Typography
+          weight="regular"
+          size="base"
+          color="primary"
+          className="ml-2 mb-4  basis-2/5"
+        >
+          {nextPaymentDate}
+        </Typography>
       </div>
-    </>
+
+      <PaymentHistoryList
+        memberPayments={paymentData}
+        onClickReceiptButton={onClickReceiptButton}
+      />
+      <Pagination
+        maxPage={maxPage}
+        currentPage={currentPage}
+        onClickPagination={onClickPagenation}
+        className="mt-6"
+      />
+    </Page>
   );
 };
