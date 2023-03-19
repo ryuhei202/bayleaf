b8c325ac7cc138ab1888f9cddbb371fede0de4a9
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-03-19 13:18:02 +0900

    fix: TabGroup配下が全てDressingPanel.tsxに入るように修正 <feature/3160/LEEAP-4087-新着こなしbldressingpanel.tsx実装>

--- src/components/pageParts/dressing/DressingPanel.tsx
+++ src/components/pageParts/dressing/DressingPanel.tsx
@@ -1,8 +1,10 @@
+import { Tab } from "@headlessui/react";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { TNonNullableDressing } from "../../../api/dressings/TDressing";
 import { StylistIdContext } from "../../../App";
 import { Button } from "../../baseParts/Button";
+import { TabAlt } from "../../baseParts/legacy/tabs/TabAlt";
 import { DressingAdvice } from "./DressingAdvice";
 import { DressingChangeItem } from "./DressingChangeItem";
 import { DressingDescription } from "./DressingDescription";
@@ -11,37 +13,47 @@ import { DressingHearing } from "./DressingHearing";
 
 type TProps = {
   readonly dressing: TNonNullableDressing;
+  readonly dressingIndex: number;
 };
-export const DressingPanel = ({ dressing }: TProps) => {
+export const DressingPanel = ({ dressing, dressingIndex }: TProps) => {
   const stylistId = useContext(StylistIdContext);
 
   const getChangeItems = (dressing: TNonNullableDressing) =>
     dressing.coordinateItems.filter((i) => i.isChangeItem).map((i) => i.item);
   return (
-    <>
-      <DressingHearing hearings={dressing.categorizedForms} />
-      <DressingDescription
-        description={dressing.description}
-        comment={dressing.comment}
-        coordinateItems={dressing.coordinateItems
-          .filter((i) => !i.isChangeItem)
-          .map((i) => i.item)}
-      />
-      <DressingAdvice advices={dressing.advices} />
-      <DressingFootwear footwear={dressing.footwear} />
-      <Button className="mb-16" variant="primary">
-        <Link to={`/consult?stylistId=${stylistId}`}>着こなしの相談をする</Link>
-      </Button>
-      {getChangeItems(dressing).length > 0 && (
-        <>
-          <DressingChangeItem changeItems={getChangeItems(dressing)} />
-          <Button variant="primary">
-            <Link to={`/consult?stylistId=${stylistId}`}>
-              着こなしの相談をする
-            </Link>
-          </Button>
-        </>
-      )}
-    </>
+    <Tab.Group>
+      <Tab.List className="w-full flex sticky top-0">
+        <TabAlt disableElevation={true} size="small" radius="small">
+          コーデ{dressingIndex + 1}
+        </TabAlt>
+      </Tab.List>
+      <Tab.Panels className="mt-10">
+        <DressingHearing hearings={dressing.categorizedForms} />
+        <DressingDescription
+          description={dressing.description}
+          comment={dressing.comment}
+          coordinateItems={dressing.coordinateItems
+            .filter((i) => !i.isChangeItem)
+            .map((i) => i.item)}
+        />
+        <DressingAdvice advices={dressing.advices} />
+        <DressingFootwear footwear={dressing.footwear} />
+        <Button className="mb-16" variant="primary">
+          <Link to={`/consult?stylistId=${stylistId}`}>
+            着こなしの相談をする
+          </Link>
+        </Button>
+        {getChangeItems(dressing).length > 0 && (
+          <>
+            <DressingChangeItem changeItems={getChangeItems(dressing)} />
+            <Button variant="primary">
+              <Link to={`/consult?stylistId=${stylistId}`}>
+                着こなしの相談をする
+              </Link>
+            </Button>
+          </>
+        )}
+      </Tab.Panels>
+    </Tab.Group>
   );
 };
--- src/pages/dressing/DressingPage.tsx
+++ src/pages/dressing/DressingPage.tsx
@@ -1,12 +1,26 @@
 import { Tab } from "@headlessui/react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { TNonNullableDressing } from "../../api/dressings/TDressing";
+import { StylistIdContext } from "../../App";
+import { Button } from "../../components/baseParts/legacy/Button";
 import { Page } from "../../components/baseParts/legacy/Page";
 import { TabAlt } from "../../components/baseParts/legacy/tabs/TabAlt";
+import { DressingAdvice } from "../../components/pageParts/dressing/DressingAdvice";
+import { DressingChangeItem } from "../../components/pageParts/dressing/DressingChangeItem";
+import { DressingDescription } from "../../components/pageParts/dressing/DressingDescription";
+import { DressingFootwear } from "../../components/pageParts/dressing/DressingFootwear";
+import { DressingHearing } from "../../components/pageParts/dressing/DressingHearing";
 
 type TProps = {
   readonly dressings: TNonNullableDressing[];
 };
 
+export const DressingPage = ({ dressings }: TProps) => {
+  const stylistId = useContext(StylistIdContext);
+
+  const getChangeItems = (dressing: TNonNullableDressing) =>
+    dressing.coordinateItems.filter((i) => i.isChangeItem).map((i) => i.item);
 
   return (
     <Page className="p-5 pb-5">
@@ -30,7 +44,31 @@ type TProps = {
         <Tab.Panels className="mt-10">
           {dressings.map((dressing, index) => (
             <Tab.Panel key={index}>
-            
+              <DressingHearing hearings={dressing.categorizedForms} />
+              <DressingDescription
+                description={dressing.description}
+                comment={dressing.comment}
+                coordinateItems={dressing.coordinateItems
+                  .filter((i) => !i.isChangeItem)
+                  .map((i) => i.item)}
+              />
+              <DressingAdvice advices={dressing.advices} />
+              <DressingFootwear footwear={dressing.footwear} />
+              <Button className="mb-16" variant="primary">
+                <Link to={`/consult?stylistId=${stylistId}`}>
+                  着こなしの相談をする
+                </Link>
+              </Button>
+              {getChangeItems(dressing).length > 0 && (
+                <>
+                  <DressingChangeItem changeItems={getChangeItems(dressing)} />
+                  <Button variant="primary">
+                    <Link to={`/consult?stylistId=${stylistId}`}>
+                      着こなしの相談をする
+                    </Link>
+                  </Button>
+                </>
+              )}
             </Tab.Panel>
           ))}
         </Tab.Panels>
