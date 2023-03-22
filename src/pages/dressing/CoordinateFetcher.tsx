33bf7ec4905b98670e02aa897ff1b4f857b5c117
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-03-22 17:08:20 +0900

    fix: 不必要な条件分岐を消去 <feature/3160/LEEAP-4090-新着こなしblcoordinatefetcher.tsx実>

--- src/pages/dressing/CoordinateFetcher.tsx
+++ src/pages/dressing/CoordinateFetcher.tsx
@@ -1,6 +1,5 @@
 import { TChartResponse } from "../../api/charts/TChartResponse";
 import { useCoordinateIndex } from "../../api/coordinates/useCoordinateIndex";
-import { Typography } from "../../components/baseParts/legacy/Typography";
 import { ErrorPage } from "../../components/baseParts/pages/ErrorPage";
 import { LoaderPage } from "../../components/baseParts/pages/LoaderPage";
 import { DressingPage } from "./DressingPage";
@@ -16,20 +15,5 @@ export const CoordinateFetcher = ({ chart }: TProps) => {
 
   if (!coordinateDatas) return <LoaderPage />;
 
-  // リリース直後には着こなしページに必要な情報がまだ存在しない可能性があるため、
-  // 以下のどれかが欠けている場合はマイページの着こなしに遷移するようにする
-  if (
-    coordinateDatas.coordinates.every(
-      (cordinate) => cordinate.id && cordinate.isReviewed && cordinate.items
-    )
-  ) {
-    return <DressingPage coordinateDatas={coordinateDatas} />;
-  }
-
-  window.location.href = `${process.env.REACT_APP_HOST_URL}/rental/plan_check`;
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Typography>リダイレクト中...</Typography>
-    </div>
-  );
+  return <DressingPage coordinateDatas={coordinateDatas} />;
 };
