8f26d07fd97eb75ede71fad47307a407603867f2
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2023-03-28 11:43:30 +0900

    feat: ランク選択フォームの実装 <feature/4077/LEEAP-4139-ランク選択bl-rankselectingform.tsx>

---
+++ src/components/pageParts/oneShot/RankSelectingForm.tsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { Button } from "../../baseParts/Button";
+import { AlertDialog } from "../../baseParts/dialogs/AlertDialog";
+import { FooterWrapper } from "../../baseParts/legacy/FooterWrapper";
+import { IconButton } from "../../baseParts/legacy/IconButton";
+import { ArrowIcon } from "../../baseParts/legacy/icons/ArrowIcon";
+import { Page } from "../../baseParts/legacy/Page";
+import { SelectButton } from "../../baseParts/legacy/SelectButton";
+import { Typography } from "../../baseParts/legacy/Typography";
+
+type TProps = {
+  readonly isSelectableBRank: boolean;
+  readonly onSelect: (value: boolean) => void;
+  readonly onSubmit: () => void;
+  readonly onCancel: () => void;
+};
+export const RankSelectingForm = ({
+  isSelectableBRank,
+  onSelect,
+  onSubmit,
+  onCancel,
+}: TProps) => {
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  return (
+    <Page className="h-screen">
+      <div className="flex flex-col justify-between h-screen">
+        <div className="px-5 pb-5">
+          <div>
+            <Typography size="xl" className="my-8" color="primary">
+              UWearでは、アイテムランク（アイテムの状態）ごとに購入料金の割引率が変わります。
+              <br />
+              Aランクのみ、Bランク混合のコーデどちらをご希望されますか？
+            </Typography>
+          </div>
+          <div>
+            <SelectButton
+              selected={!isSelectableBRank}
+              onClick={() => onSelect(!isSelectableBRank)}
+            >
+              Aランクでコーデを作ってほしい
+            </SelectButton>
+            <SelectButton
+              selected={isSelectableBRank}
+              onClick={() => onSelect(isSelectableBRank)}
+              className="mt-6"
+            >
+              Bランクアイテムがあれば、
+              <br /> Bランク混合でコーデを作ってほしい
+            </SelectButton>
+            <Typography size="xs" color="gray" className="mt-2">
+              ※必ずBランクをお届けするという保証ではございません。
+            </Typography>
+          </div>
+
+          <Typography
+            className="my-8 text-center underline"
+            color="red"
+            onClick={() => setIsDialogOpen(true)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 01-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 01-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 01-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584zM12 18a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Bランクとは
+          </Typography>
+        </div>
+        <FooterWrapper className="px-3 py-4">
+          <div className="flex flex-row">
+            <IconButton className="flex-none" onClick={onCancel}>
+              <ArrowIcon className="h-10 my-auto" />
+            </IconButton>
+            <Button size="none" className="grow ml-3" onClick={onSubmit}>
+              <Typography className="my-auto">次へ</Typography>
+            </Button>
+          </div>
+        </FooterWrapper>
+      </div>
+      {isDialogOpen && (
+        <AlertDialog
+          open={isDialogOpen}
+          title="Bランクとは？"
+          description={
+            <>
+              少し使用感があり、比較的目立たない気になる傷や汚れがあるアイテムです。
+              <br />
+              着用上問題ないと判断したアイテムを通常より購入できます。
+              <br />
+              {/* TODO: URL出来次第、hrefに差し込む
+              <a href="" className="font-bold underline">
+                詳しくはこちら
+              </a>
+               */}
+            </>
+          }
+          onClose={() => setIsDialogOpen(false)}
+          onClick={() => setIsDialogOpen(false)}
+        />
+      )}
+    </Page>
+  );
+};
