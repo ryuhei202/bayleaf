fda2c611e5e02277ee4d8732a712270973a8a519
Onishi_Kengo <k-ounishi@kiizan-kiizan.co.jp>
2023-04-07 15:52:26 +0900

    TCoordinateIndexResponseの型を変える <feature/LEEAP-4198-着こなしページランク表示blレスポンスの共通化に対応>

--- src/api/shared/TItemResponse.ts
+++ src/api/shared/TItemResponse.ts
@@ -1,13 +1,11 @@
+import { TImagePathsResponse } from "./TImagePathsResponse";
+
 export type TItemResponse = {
   readonly id: number;
+  readonly isTops: boolean;
   readonly isPurchased: boolean;
   readonly brandName: string;
-  readonly imagePaths: {
-    original: string;
-    large: string;
-    largeThumb: string | null;
-    thumb: string;
-  };
+  readonly imagePaths: TImagePathsResponse;
   readonly categoryName: string;
   readonly colorName: string;
   readonly price: number;
