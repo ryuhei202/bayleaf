fe3950bff4e1acf08a48ed3a1deba7e0b03bd52e
Onishi_Kengo <k-ounishi@kiizan-kiizan.co.jp>
2023-04-07 15:38:15 +0900

    TChartItemsIndexResponseの型を変える <feature/LEEAP-4198-着こなしページランク表示blレスポンスの共通化に対応>

--- src/api/chartItems/useChartItemsIndex.ts
+++ src/api/chartItems/useChartItemsIndex.ts
@@ -1,12 +1,12 @@
-import { TItemResponse } from "../shared/TItemResponse";
 import { useGetRequest } from "../useGetRequest";
+import { TChartItemsIndexResponse } from "./TChartItemsIndexResponse";
 
 type TArgs = {
   readonly chartId: number;
 };
 
 export const useChartItemsIndex = ({ chartId }: TArgs) => {
-  const { data, error } = useGetRequest<TItemResponse[]>(
+  const { data, error } = useGetRequest<TChartItemsIndexResponse>(
     `charts/${chartId}/chart_items`
   );
   return {
