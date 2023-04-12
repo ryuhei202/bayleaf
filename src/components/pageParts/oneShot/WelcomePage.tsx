303acdbc31ed4f80cff7ab7b1231da3d18ef3fcc
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-04-12 12:38:01 +0900

    fix: discountPriceをpropsとして受け取るように修正。discountPriceを取得するロジックをOneShotContainerに移動 <feature/3979/LEEAP-4241-初回割引bloneshotstartingconfirm修>

--- src/components/pageParts/oneShot/WelcomePage.tsx
+++ src/components/pageParts/oneShot/WelcomePage.tsx
@@ -1,8 +1,8 @@
-import { TSerialCodesIndexResponse } from "../../../api/serialCodes/TSerialCodesIndexResponse";
 import FIRST_CLOTH from "../../../images/icons/cloths/1.svg";
 import THIRD_CLOTH from "../../../images/icons/cloths/3.svg";
 import FORTH_CLOTH from "../../../images/icons/cloths/4.svg";
 import DiagonalLineIcon from "../../../images/icons/diagonal-line.svg";
+import { campaignIndex } from "../../../models/shared/Campaign";
 import { OneShot } from "../../../models/shared/OneShot";
 import { withTax } from "../../../models/shared/Tax";
 
@@ -12,28 +12,11 @@ import { Typography } from "../../baseParts/legacy/Typography";
 import { ScheduleDiagram } from "./ScheduleDiagram";
 
 type TProps = {
-  readonly serialCodesIndexData: TSerialCodesIndexResponse[];
+  readonly discountPrice: number | undefined;
   readonly onClickStart: () => void;
 };
 
-const campaignIndex = [
-  {
-    firstTimeOneShotSerialCampaignId: 224,
-    campaignWord: "初回キャンペーン!!",
-  },
-];
-
-export const WelcomePage = ({ serialCodesIndexData, onClickStart }: TProps) => {
-  const targetCampaign = serialCodesIndexData.find(
-    (campaign) =>
-      campaign.mSerialCampaignId ===
-      campaignIndex[0].firstTimeOneShotSerialCampaignId
-  );
-
-  const discountPrice = targetCampaign
-    ? targetCampaign.discountPrice
-    : undefined;
-
+export const WelcomePage = ({ discountPrice, onClickStart }: TProps) => {
   return (
     <Page className="flex flex-col h-full min-h-screen justify-between items-center text-themeGray pt-8 px-3 pb-3">
       <Typography size="2xl" className="text-center mb-8">
