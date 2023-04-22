11c55f7894de02527f5bb7e4804d6d549cf57c3c
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-04-22 12:40:28 +0900

    feat: ポイント履歴のリスト作成 <feature/4128/LEEAP-4293-bayleeafポイント合計historylist.tsx>

---
+++ src/components/pageParts/pointHistory/PointHistoryList.tsx
@@ -0,0 +1,47 @@
+import { Typography } from "../../baseParts/legacy/Typography";
+import { PointHistoryListItem } from "./PointHistoryListItem";
+
+type TMemberPoint = {
+  readonly id: number;
+  readonly createdAt: string;
+  readonly point: number;
+};
+
+type TProps = {
+  memberPoints: TMemberPoint[];
+};
+
+export const PointHistoryList = ({ memberPoints }: TProps) => {
+  return (
+    <div>
+      <div className="flex">
+        <div className="basis-1/3 ml-6">
+          <Typography color="strong-gray" size="xs">
+            日付
+          </Typography>
+        </div>
+        <div className="basis-1/3 ml-3">
+          <Typography color="strong-gray" size="xs" className="ml-[4px]">
+            区分
+          </Typography>
+        </div>
+        <div className="ml-2">
+          <Typography color="strong-gray" size="xs" className="ml-[8px]">
+            ポイント
+          </Typography>
+        </div>
+      </div>
+      <div>
+        {memberPoints.map((memberPoint: TMemberPoint) => {
+          return (
+            <PointHistoryListItem
+              key={memberPoint.id}
+              point={memberPoint.point}
+              pointCreatedAt={memberPoint.createdAt}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
