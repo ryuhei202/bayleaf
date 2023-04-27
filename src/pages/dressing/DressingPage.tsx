7d12ae38be1bf96c429cb67d06879e13daf5aa33
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-04-27 18:05:38 +0900

    fix: コーデが一つの場合はタブを表示させない様に修正 <release/LEEAP-4149-着こなしページのデザインを修正する>

--- src/pages/dressing/DressingPage.tsx
+++ src/pages/dressing/DressingPage.tsx
@@ -13,14 +13,17 @@ export const DressingPage = ({ coordinateDatas }: TProps) => {
     <>
       <Page className="px-5 pb-5">
         <Tab.Group>
-          <Tab.List className="w-full flex justify-center sticky top-0 z-10">
-            {coordinateDatas.coordinates.map((_, index) => (
-              <TabMenu className="basis-1/2 text-xl" key={index}>
-                コーデ 0{index + 1}
-              </TabMenu>
-            ))}
-          </Tab.List>
-
+          {coordinateDatas.coordinates.length > 1 ? (
+            <Tab.List className="w-full flex justify-center sticky top-0 z-10">
+              {coordinateDatas.coordinates.map((_, index) => (
+                <TabMenu className="basis-1/2 text-xl" key={index}>
+                  コーデ 0{index + 1}
+                </TabMenu>
+              ))}
+            </Tab.List>
+          ) : (
+            <></>
+          )}
           <Tab.Panels className="mt-10">
             {coordinateDatas.coordinates.map((coordinateData, index) => (
               <Tab.Panel key={index}>
