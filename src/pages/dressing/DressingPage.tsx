1a23af9a4f4942b9ffa36647b86ec23781aaa990
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-04-27 18:07:46 +0900

    fix: 不必要なフラグメントを消去 <release/LEEAP-4149-着こなしページのデザインを修正する>

--- src/pages/dressing/DressingPage.tsx
+++ src/pages/dressing/DressingPage.tsx
@@ -10,29 +10,27 @@ type TProps = {
 
 export const DressingPage = ({ coordinateDatas }: TProps) => {
   return (
-    <>
-      <Page className="px-5 pb-5">
-        <Tab.Group>
-          {coordinateDatas.coordinates.length > 1 ? (
-            <Tab.List className="w-full flex justify-center sticky top-0 z-10">
-              {coordinateDatas.coordinates.map((_, index) => (
-                <TabMenu className="basis-1/2 text-xl" key={index}>
-                  コーデ 0{index + 1}
-                </TabMenu>
-              ))}
-            </Tab.List>
-          ) : (
-            <></>
-          )}
-          <Tab.Panels className="mt-10">
-            {coordinateDatas.coordinates.map((coordinateData, index) => (
-              <Tab.Panel key={index}>
-                <DressingPanelFetcher coordinateId={coordinateData.id} />
-              </Tab.Panel>
+    <Page className="px-5 pb-5">
+      <Tab.Group>
+        {coordinateDatas.coordinates.length > 1 ? (
+          <Tab.List className="w-full flex justify-center sticky top-0 z-10">
+            {coordinateDatas.coordinates.map((_, index) => (
+              <TabMenu className="basis-1/2 text-xl" key={index}>
+                コーデ 0{index + 1}
+              </TabMenu>
             ))}
-          </Tab.Panels>
-        </Tab.Group>
-      </Page>
-    </>
+          </Tab.List>
+        ) : (
+          <></>
+        )}
+        <Tab.Panels className="mt-10">
+          {coordinateDatas.coordinates.map((coordinateData, index) => (
+            <Tab.Panel key={index}>
+              <DressingPanelFetcher coordinateId={coordinateData.id} />
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </Page>
   );
 };
