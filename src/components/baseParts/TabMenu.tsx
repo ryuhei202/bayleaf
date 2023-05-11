4c92801249b510ab37b082980bf377b2a1f09754
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-05-11 18:48:04 +0900

    fix: タブのui崩れを修正 <hotfix/LEEAP-4388-hotfixタブのuiが崩れているため修正する>

--- src/components/baseParts/TabMenu.tsx
+++ src/components/baseParts/TabMenu.tsx
@@ -10,19 +10,17 @@ export const TabMenu = ({ children, className, onChangeIsConfirm }: TProps) => {
   return (
     <Tab as={Fragment}>
       {({ selected }) => (
-        <div className="bg-clay w-full pt-5">
-          <div
-            className={`flex justify-center items-center mx-1 ${
-              selected
-                ? "border-b-2 border-themeGray"
-                : "border-b border-[#C7C9C4]"
-            } ${className}`}
-            onClick={onChangeIsConfirm}
-          >
-            <p className={`my-3 ${selected ? "opacity-100" : "opacity-20"}`}>
-              {children}
-            </p>
-          </div>
+        <div
+          className={`bg-clay w-full pt-5 flex justify-center items-center px-1 ${
+            selected
+              ? "border-b-2 border-themeGray"
+              : "border-b border-[#C7C9C4]"
+          } ${className}`}
+          onClick={onChangeIsConfirm}
+        >
+          <p className={`my-3 ${selected ? "opacity-100" : "opacity-20"}`}>
+            {children}
+          </p>
         </div>
       )}
     </Tab>
