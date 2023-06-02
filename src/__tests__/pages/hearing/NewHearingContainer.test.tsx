50605aa836cb7bfb3a894fb79ece9eff034e94f3
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2023-06-02 18:43:38 +0900

    wip: NewHearingContainerのテストケースを実装 <feature/4467/LEEAP-4472-bヒアリングbl_newhearingcontainerの>

---
+++ src/__tests__/pages/hearing/NewHearingContainer.test.tsx
@@ -0,0 +1,91 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { ReactElement } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { TNotNullPlanIdMember } from "../../../api/members/TMembersIndexResponse";
+import { getHearingFormShowMock } from "../../../mocks/hearingForms/getHearingFormShowMock";
+import { server } from "../../../mocks/server";
+import { M_PLAN_IDS } from "../../../models/shared/Plans";
+import { NewHearingContainer } from "../../../pages/hearing/NewHearingContainer";
+import { createQueryWrapper } from "../../utils/MockProvider";
+
+const { queryWrapper: QueryWrapper } = createQueryWrapper();
+const AllTheProviders = ({
+  children,
+}: {
+  children: React.ReactElement;
+}): ReactElement => {
+  return (
+    <BrowserRouter>
+      <QueryWrapper>{children}</QueryWrapper>
+    </BrowserRouter>
+  );
+};
+
+const createMember = (mPlanId: number): TNotNullPlanIdMember => {
+  return {
+    id: 1,
+    email: "stg-replication@kiizan-kiizan.co,jp",
+    nextPaymentDate: "2022-11-1",
+    mPlanId,
+    isLatestChartDelivered: true,
+    isReturnRequired: false,
+    isFirstTime: true,
+    isSuspend: false,
+    isPaymentError: false,
+    rentalRemainingNum: 1,
+    requestedPlanId: null,
+    point: 2000,
+  };
+};
+
+const createHearingFormResponse = () => {
+  return {
+    id: 1,
+    categoryId: 1,
+    categoryName: "使いたいシーン",
+    multipleAnswerNextFormId: null,
+    title: "借りたコーデをどちらの予定で使いますか？",
+    options: [
+      {
+        id: 1,
+        name: "日常の予定",
+        nextFormId: null,
+        isText: false,
+        isSingleAnswer: true,
+      },
+    ],
+  };
+};
+
+describe("NewHearingContainer.tsx", () => {
+  test("ライトプランで全てのヒアリングに回答し終えたら、RankSelectingFormが表示される。", async () => {
+    // Arrange
+    server.use(
+      getHearingFormShowMock({
+        status: 200,
+        hearingFormId: 1,
+        response: createHearingFormResponse(),
+      })
+    );
+    // Act
+    render(
+      <NewHearingContainer
+        nextPlanId={M_PLAN_IDS.LIGHT}
+        member={createMember(M_PLAN_IDS.LIGHT)}
+      />,
+      {
+        wrapper: AllTheProviders,
+      }
+    );
+    const user = userEvent.setup();
+
+    expect(
+      await screen.findByTestId("beforeHearingConfirm")
+    ).toBeInTheDocument();
+
+    user.click(screen.getByTestId("BuyItemSelectSubmitButton"));
+
+    expect(await screen.findByTestId("rankSelectingForm")).toBeInTheDocument();
+  });
+});
